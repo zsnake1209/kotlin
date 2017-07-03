f4187a5edcee1424346b8a9483067c3f3dc230e3
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-07-03 16:11:40 +0300

    [NI] patch design: resolve block with not fixed variable
    Simplified:
    
    fun enhance(operation: (Int) -> Sample) {
        val create: (Int) -> Sample = run {
            { index -> operation(index) } // can't infer type for `index`
        }
    }
    
    class Sample

--- core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/typeEnhancement/signatureEnhancement.kt
+++ core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/typeEnhancement/signatureEnhancement.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -83,7 +83,7 @@ private class SignatureParts(
     fun enhance(predefined: TypeEnhancementInfo? = null): PartEnhancementResult {
         val qualifiers = fromOverride.computeIndexedQualifiersForOverride(this.fromOverridden, isCovariant)
 
-        val qualifiersWithPredefined: ((Int) -> JavaTypeQualifiers)? = predefined?.let {
+        val qualifiersWithPredefined: ((Int) -> JavaTypeQualifiers)? = predefined?.let<TypeEnhancementInfo?, ((Int) -> JavaTypeQualifiers)?> {
             {
                 index -> predefined.map[index] ?: qualifiers(index)
             }
