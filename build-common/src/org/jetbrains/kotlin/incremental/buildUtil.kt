90035d041ec54afea8eb542b248ff9b3f9e520ed
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-07-07 15:57:01 +0300

    [NI] patch: type mismatch, investigate
    buildUtil.kt:166:12: error: type mismatch: inferred type is List<Any?> but List<GeneratedFile<Target>> was expected

--- build-common/src/org/jetbrains/kotlin/incremental/buildUtil.kt
+++ build-common/src/org/jetbrains/kotlin/incremental/buildUtil.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -173,7 +173,7 @@ fun<Target> OutputItemsCollectorImpl.generatedFiles(
             "class" -> GeneratedJvmClass(target, outputItem.sourceFiles, outputItem.outputFile)
             else -> GeneratedFile(target, outputItem.sourceFiles, outputItem.outputFile)
         }
-    }
+    } as List<GeneratedFile<Target>>
 }
 
 data class DirtyData(
