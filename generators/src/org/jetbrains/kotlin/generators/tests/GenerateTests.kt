eba870d07856fb0851508e7a462bc4f01a867f63
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-07-31 23:07:06 +0300

    Add JS lookup tracker tests

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1194,6 +1194,8 @@ fun main(args: Array<String>) {
         testClass<AbstractJvmLookupTrackerTest> {
             model("incremental/lookupTracker/jvm", extension = null, recursive = false)
         }
+        testClass<AbstractJsLookupTrackerTest> {
+            model("incremental/lookupTracker/js", extension = null, recursive = false)
         }
 
         testClass(AbstractIncrementalLazyCachesTest::class.java) {
