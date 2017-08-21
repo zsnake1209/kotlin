17f5c1f5d2003cb7ae1ba941042a67b94bed8b4e
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-08-21 06:32:15 +0300

    ~ remove unnecessary withIC call

--- compiler/daemon/src/org/jetbrains/kotlin/daemon/CompileServiceImpl.kt
+++ compiler/daemon/src/org/jetbrains/kotlin/daemon/CompileServiceImpl.kt
@@ -404,11 +404,9 @@ class CompileServiceImpl(
                     CompileService.TargetPlatform.JS -> {
                         val k2jsArgs = k2PlatformArgs as K2JSCompilerArguments
 
-                        withIC {
-                            withJsIC {
-                                doCompile(sessionId, daemonReporter, tracer = null) { _, _ ->
-                                    execJsIncrementalCompiler(k2jsArgs, gradleIncrementalArgs, gradleIncrementalServicesFacade, compilationResults!!, messageCollector)
-                                }
+                        withJsIC {
+                            doCompile(sessionId, daemonReporter, tracer = null) { _, _ ->
+                                execJsIncrementalCompiler(k2jsArgs, gradleIncrementalArgs, gradleIncrementalServicesFacade, compilationResults!!, messageCollector)
                             }
                         }
                     }
