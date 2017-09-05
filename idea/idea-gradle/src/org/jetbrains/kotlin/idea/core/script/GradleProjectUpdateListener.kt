b4125d7b0446a4b158bd9d0418cd252585a8dbf7
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-09-05 21:31:20 +0300

    Do not update script dependencies unless there was a successful gradle sync

---
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/core/script/GradleProjectUpdateListener.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.idea.core.script
+
+import com.intellij.openapi.components.service
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
+import org.jetbrains.plugins.gradle.util.GradleConstants
+
+class GradleProjectUpdateListener : ExternalSystemTaskNotificationListenerAdapter() {
+    override fun onSuccess(id: ExternalSystemTaskId) {
+        if (id.type == ExternalSystemTaskType.RESOLVE_PROJECT && id.projectSystemId == GradleConstants.SYSTEM_ID) {
+            id.findProject()?.service<GradleProjectUpdateTracker>()?.incModificationCount()
+        }
+    }
+}
