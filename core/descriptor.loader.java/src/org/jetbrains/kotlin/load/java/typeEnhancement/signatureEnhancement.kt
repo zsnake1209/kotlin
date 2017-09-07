726d18f4f6c5cb2e5cb652dd21ba5c04dcd69c9c
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-09-07 14:04:47 +0300

    Minor. Simplify handling nullablility for warning

--- core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/typeEnhancement/signatureEnhancement.kt
+++ core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/typeEnhancement/signatureEnhancement.kt
@@ -314,7 +314,6 @@ class SignatureEnhancement(private val annotationTypeQualifierResolver: Annotati
             val nullabilityFromSupertypesWithWarning = fromSupertypes
                     .mapNotNull { it.unwrapEnhancement().extractQualifiers().nullability }
                     .toSet()
-                    .takeIf { it != nullabilityFromSupertypes }
 
             val own = extractQualifiersFromAnnotations(isHeadTypeConstructor)
             val isAnyNonNullTypeParameter = own.isNotNullTypeParameter || superQualifiers.any { it.isNotNullTypeParameter }
@@ -350,10 +349,10 @@ class SignatureEnhancement(private val annotationTypeQualifierResolver: Annotati
             val nullability = nullabilityFromSupertypes.select(NullabilityQualifier.NOT_NULL, NullabilityQualifier.NULLABLE, ownNullability)
             val mutability = mutabilityFromSupertypes.select(MutabilityQualifier.MUTABLE, MutabilityQualifier.READ_ONLY, own.mutability)
 
-            val canChange = ownNullabilityForWarning != ownNullability || nullabilityFromSupertypesWithWarning != null
+            val canChange = ownNullabilityForWarning != ownNullability || nullabilityFromSupertypesWithWarning != nullabilityFromSupertypes
             if (nullability == null && canChange) {
-                val nullabilityWithWarning = (nullabilityFromSupertypesWithWarning ?: nullabilityFromSupertypes)
-                        .select(NullabilityQualifier.NOT_NULL, NullabilityQualifier.NULLABLE, ownNullabilityForWarning)
+                val nullabilityWithWarning =
+                        nullabilityFromSupertypesWithWarning.select(NullabilityQualifier.NOT_NULL, NullabilityQualifier.NULLABLE, ownNullabilityForWarning)
 
                 return createJavaTypeQualifiers(nullabilityWithWarning, mutability, true)
             }
