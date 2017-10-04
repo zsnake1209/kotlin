6cead8b00409925ee49a9a96c904b4b831973a52
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2017-10-04 14:03:23 +0300

    Add annotation for default parameter value

---
+++ compiler/testData/codegen/box/signatureAnnotations/defaultWithKotlinBase.kt
@@ -0,0 +1,23 @@
+// IGNORE_BACKEND: JS, NATIVE
+
+// FILE: A.kt
+open class A {
+    open fun x(x: Int = foo()) = x
+    private fun foo() = 42
+}
+
+// FILE: B.java
+public class B extends A {
+    public int x(int i) {
+        return i + 1;
+    }
+}
+
+// FILE: box.kt
+fun box(): String {
+    if (B().x() != 43) {
+        return "FAIL"
+    }
+
+    return "OK"
+}
