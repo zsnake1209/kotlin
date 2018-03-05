75cc78796c9c2a29d0ed7dcbf5defd9328c55ea7
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-03-05 18:31:03 +0300

    Fix comparing multiline diagnostic parameters
     This change helps to check diagnostic messages that ends with new line.
     For example:
       <!DIAGNOSTIC(a
    ; b)!>
    
    Because of trim we had actual parameter just 'a' and expected parameter 'a\n'

--- compiler/frontend/src/org/jetbrains/kotlin/checkers/CheckerTestUtil.java
+++ compiler/frontend/src/org/jetbrains/kotlin/checkers/CheckerTestUtil.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.checkers;
@@ -839,7 +828,7 @@ public class CheckerTestUtil {
             List<String> parsedParameters = new SmartList<>();
             Matcher parametersMatcher = INDIVIDUAL_PARAMETER_PATTERN.matcher(parameters);
             while (parametersMatcher.find())
-                parsedParameters.add(unescape(parametersMatcher.group().trim()));
+                parsedParameters.add(unescape(StringsKt.trimStart(parametersMatcher.group(), ' ')));
             return new TextDiagnostic(name, platform, parsedParameters, inference);
         }
 
