facd53607853c4080d898abd1ab94ebb3a9a1221
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-06-06 12:31:31 +0300

    ~ test fixes

--- compiler/testData/asJava/lightClasses/nullabilityAnnotations/IntOverridesAny.ide.java
+++ compiler/testData/asJava/lightClasses/nullabilityAnnotations/IntOverridesAny.ide.java
@@ -1,10 +1,8 @@
 public final class C implements Tr {
-    private final int v = 1;
+    private final int v;
 
-    @org.jetbrains.annotations.NotNull
     public java.lang.Integer foo() { /* compiled code */ }
 
-    @org.jetbrains.annotations.NotNull
     public java.lang.Integer getV() { /* compiled code */ }
 
     public C() { /* compiled code */ }
--- compiler/testData/asJava/lightClasses/object/SimpleObject.ide.java
+++ compiler/testData/asJava/lightClasses/object/SimpleObject.ide.java
@@ -1,17 +1,15 @@
 public final class A {
-    private static final int c = 1;
-    @org.jetbrains.annotations.NotNull
+    private static final int c;
     private static java.lang.String v;
     public static final pack.A INSTANCE;
 
     public final int getC() { /* compiled code */ }
 
-    @org.jetbrains.annotations.NotNull
     public final java.lang.String getV() { /* compiled code */ }
 
-    public final void setV(@org.jetbrains.annotations.NotNull java.lang.String p) { /* compiled code */ }
+    public final void setV(java.lang.String deferredType8) { /* compiled code */ }
 
-    public final int f() { /* compiled code */ }
+    public final int1 f() { /* compiled code */ }
 
     private A() { /* compiled code */ }
 }
