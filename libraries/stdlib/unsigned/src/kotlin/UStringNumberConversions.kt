aa8b40a9739eca7d48100b65fa2d8e94fb701bd2
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-08-16 14:59:36 +0300

    Make unsigned toString functions inline - fails tests

--- libraries/stdlib/unsigned/src/kotlin/UStringNumberConversions.kt
+++ libraries/stdlib/unsigned/src/kotlin/UStringNumberConversions.kt
@@ -13,7 +13,7 @@ package kotlin.text
 @SinceKotlin("1.3")
 @ExperimentalUnsignedTypes
 //@kotlin.internal.InlineOnly
-public /*inline*/ fun UByte.toString(radix: Int): String = this.toInt().toString(radix)
+public inline fun UByte.toString(radix: Int): String = this.toInt().toString(radix)
 
 /**
  * Returns a string representation of this [Short] value in the specified [radix].
@@ -23,7 +23,7 @@ public /*inline*/ fun UByte.toString(radix: Int): String = this.toInt().toString
 @SinceKotlin("1.3")
 @ExperimentalUnsignedTypes
 //@kotlin.internal.InlineOnly
-public /*inline*/ fun UShort.toString(radix: Int): String = this.toInt().toString(radix)
+public inline fun UShort.toString(radix: Int): String = this.toInt().toString(radix)
 
 
 /**
@@ -34,7 +34,7 @@ public /*inline*/ fun UShort.toString(radix: Int): String = this.toInt().toStrin
 @SinceKotlin("1.3")
 @ExperimentalUnsignedTypes
 //@kotlin.internal.InlineOnly
-public /*inline*/ fun UInt.toString(radix: Int): String = this.toLong().toString(radix)
+public inline fun UInt.toString(radix: Int): String = this.toLong().toString(radix)
 
 /**
  * Returns a string representation of this [Long] value in the specified [radix].
