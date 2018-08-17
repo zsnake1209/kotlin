1c86548eac1e37ee446c2eb3c82de05cb544df01
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-08-17 16:30:45 +0300

    ~ update DCE data

--- js/js.translator/testData/box/expression/cast/checkThrowCCE.kt
+++ js/js.translator/testData/box/expression/cast/checkThrowCCE.kt
@@ -1,5 +1,5 @@
 // IGNORE_BACKEND: JS_IR
-// EXPECTED_REACHABLE_NODES: 1094
+// EXPECTED_REACHABLE_NODES: 1208
 package foo
 
 import kotlin.reflect.*
--- js/js.translator/testData/box/reflection/external.kt
+++ js/js.translator/testData/box/reflection/external.kt
@@ -1,5 +1,5 @@
 // IGNORE_BACKEND: JS_IR
-// EXPECTED_REACHABLE_NODES: 1099
+// EXPECTED_REACHABLE_NODES: 1212
 external class A
 
 external object O
