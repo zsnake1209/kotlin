f6fec8ae1be7229bc50a7a70fded4670e22f4c45
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2018-09-05 16:18:05 +0300

    Avoid putting a constant on the stack twice in intrinsic

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/IrIllegalArgumentException.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/IrIllegalArgumentException.kt
@@ -18,14 +18,22 @@ package org.jetbrains.kotlin.backend.jvm.intrinsics
 
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.codegen.AsmUtil.genThrow
+import org.jetbrains.kotlin.codegen.AsmUtil.pop
 import org.jetbrains.kotlin.ir.expressions.IrConst
 import org.jetbrains.kotlin.ir.expressions.IrMemberAccessExpression
 import org.jetbrains.kotlin.resolve.jvm.jvmSignature.JvmMethodSignature
+import org.jetbrains.org.objectweb.asm.Type
 
 class IrIllegalArgumentException : IntrinsicMethod() {
-    override fun toCallable(expression: IrMemberAccessExpression, signature: JvmMethodSignature, context: JvmBackendContext): IrIntrinsicFunction {
+    override fun toCallable(
+        expression: IrMemberAccessExpression,
+        signature: JvmMethodSignature,
+        context: JvmBackendContext
+    ): IrIntrinsicFunction {
         return IrIntrinsicFunction.create(expression, signature, context) {
             val message = (expression.getValueArgument(0) as IrConst<String>).value
+            // Avoid putting the string constant on the stack twice; pop will optimize one copy away.
+            pop(it, Type.getType("java/lang/String"))
             genThrow(it, "java/lang/IllegalArgumentException", message)
         }
     }
