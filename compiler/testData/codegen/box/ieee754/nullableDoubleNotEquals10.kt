3a983e776a236b014091df56b24c6a6e8766adbf
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-09-14 17:58:13 +0300

    ~ whoops

--- compiler/testData/codegen/box/ieee754/nullableDoubleNotEquals10.kt
+++ compiler/testData/codegen/box/ieee754/nullableDoubleNotEquals10.kt
@@ -1,34 +1,26 @@
 // LANGUAGE_VERSION: 1.0
-//fun myNotEquals(a: Double?,
-// b: Double) = a != b
+fun myNotEquals(a: Double?, b: Double?) = a != b
 
-inline fun foo(init: (Int) -> Unit) {
-    var i = 0
-    while (i < 10) {
-        init(i)
-        ++i
-    }
-}
+fun myNotEquals1(a: Double?, b: Double) = a != b
 
+fun myNotEquals2(a: Double, b: Double?) = a != b
+
+fun myNotEquals0(a: Double, b: Double) = a != b
 
 
 fun box(): String {
-    foo { it * it }
+    if (myNotEquals(null, null)) return "fail 1"
+    if (!myNotEquals(null, 0.0)) return "fail 2"
+    if (!myNotEquals(0.0, null)) return "fail 3"
+    if (myNotEquals(0.0, 0.0)) return "fail 4"
 
-    Array<Int>(10) { it }
+    if (!myNotEquals1(null, 0.0)) return "fail 5"
+    if (myNotEquals1(0.0, 0.0)) return "fail 6"
 
-//    if (myNotEquals(null, null)) return "fail 1"
-//    if (!myNotEquals(null, 0.0)) return "fail 2"
-//    if (!myNotEquals(0.0, null)) return "fail 3"
-//    if (myNotEquals(0.0, 0.0)) return "fail 4"
-//
-//    if (!myNotEquals1(null, 0.0)) return "fail 5"
-//    if (myNotEquals1(0.0, 0.0)) return "fail 6"
-//
-//    if (!myNotEquals2(0.0, null)) return "fail 7"
-//    if (myNotEquals2(0.0, 0.0)) return "fail 8"
-//
-//    if (myNotEquals0(0.0, 0.0)) return "fail 9"
+    if (!myNotEquals2(0.0, null)) return "fail 7"
+    if (myNotEquals2(0.0, 0.0)) return "fail 8"
+
+    if (myNotEquals0(0.0, 0.0)) return "fail 9"
 
     return "OK"
 }
