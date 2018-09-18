fd2b0b97e657534e20dbc2c514ba70a89a9da644
Sergey Igushkin <hotkeytlt@gmail.com>
2018-09-18 12:53:17 +0300

    Support parent attributes transformation in HierarchyAttributeContainer

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/HierarchyAttributeContainer.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/HierarchyAttributeContainer.kt
@@ -19,20 +19,42 @@ import org.gradle.api.attributes.Attribute
 import org.gradle.api.attributes.AttributeContainer
 import java.util.*
 
+abstract class AttributeTransformer {
+    abstract fun <T> transform(key: Attribute<T>, value: T): T?
+
+    companion object {
+        fun create(transformFunction: (key: Attribute<*>, value: Any?) -> Any?) = object : AttributeTransformer() {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T> transform(key: Attribute<T>, value: T): T? =
+                key.type.cast(transformFunction(key, value))
+        }
+    }
+}
+
+fun <T> AttributeTransformer.fetchAndTransform(key: Attribute<T>, attributeContainer: AttributeContainer): T? {
+    // Type safety wrapper, captures `T`
+    val value = attributeContainer.getAttribute(key) ?: return null
+    return transform(key, value)
+}
+
 // TODO better implementation: attribute invariants (no attrs with same name and different types allowed), thread safety?
-class HierarchyAttributeContainer(val parent: AttributeContainer?) : AttributeContainer {
+class HierarchyAttributeContainer(
+    val parent: AttributeContainer?,
+    private val transformParentAttributes: AttributeTransformer = AttributeTransformer.create { _, value -> value }
+) : AttributeContainer {
     private val attributesMap = Collections.synchronizedMap(mutableMapOf<Attribute<*>, Any>())
 
     override fun contains(key: Attribute<*>): Boolean =
-        attributesMap.contains(key) || parent?.contains(key) ?: false
+        attributesMap.contains(key) || parent?.let { transformParentAttributes.fetchAndTransform(key, it) } != null
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : Any?> getAttribute(key: Attribute<T>?): T? =
-        attributesMap.get(key as Attribute<*>) as T? ?: parent?.getAttribute(key)
+    override fun <T : Any?> getAttribute(key: Attribute<T>): T? =
+        attributesMap.get(key) as T? ?: parent?.let { transformParentAttributes.fetchAndTransform(key, it) }
 
     override fun isEmpty(): Boolean = attributesMap.isEmpty() && parent?.isEmpty ?: false
 
-    override fun keySet(): Set<Attribute<*>> = attributesMap.keys + parent?.keySet().orEmpty()
+    override fun keySet(): Set<Attribute<*>> = attributesMap.keys +
+            parent?.keySet()?.filter { transformParentAttributes.fetchAndTransform(it, parent) != null }.orEmpty()
 
     override fun <T : Any?> attribute(key: Attribute<T>?, value: T): AttributeContainer {
         val checkedValue = requireNotNull(value as Any?) { "null values for attributes are not supported" }
