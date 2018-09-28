8f816fbd6d8b3121f2e4da100ad10aa08614932e
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-09-28 17:03:20 +0300

    Revert: Forbid suspension points in critical sections

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -2433,11 +2433,6 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
             return;
         }
 
-        if (resolvedCall.getResultingDescriptor() instanceof FunctionDescriptor &&
-            ((FunctionDescriptor) resolvedCall.getResultingDescriptor()).isSuspend()) {
-            state.getGlobalCoroutinesContext().checkSuspendCall(resolvedCall);
-        }
-
         boolean isSuspendNoInlineCall =
                 CoroutineCodegenUtilKt.isSuspendNoInlineCall(resolvedCall, this, state.getLanguageVersionSettings());
         boolean isConstructor = resolvedCall.getResultingDescriptor() instanceof ConstructorDescriptor;
