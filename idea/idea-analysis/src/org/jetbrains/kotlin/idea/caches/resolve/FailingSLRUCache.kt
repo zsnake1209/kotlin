50bdc6eb282547c6a56f9f17565a08e154e0764a
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-11-06 19:58:49 +0300

    Implement SLRUCache with purge queue

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/FailingSLRUCache.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/FailingSLRUCache.kt
@@ -5,24 +5,53 @@
 
 package org.jetbrains.kotlin.idea.caches.resolve
 
+import com.intellij.util.Consumer
+import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.containers.SLRUCache
 import java.util.*
 
-abstract class FailingSLRUCache<K, V>(a: Int, b: Int) : SLRUCache<K, V>(a, b) {
+abstract class FailingSLRUCache<K, V>(a: Int, b: Int) : StableSLRUCache<K, V>(a, b) {
 
     val random = Random(12345)
+    val prob = 0.3
+
 
     override fun get(key: K): V {
-        if (random.nextFloat() > 0.7) {
+        if (random.nextFloat() <= prob) {
             this.remove(key)
         }
         return super.get(key)
     }
 
     override fun getIfCached(key: K): V? {
-        if (random.nextFloat() > 0.7) {
+        if (random.nextFloat() <= prob) {
             this.remove(key)
         }
         return super.getIfCached(key)
+    }
+}
+
+abstract class StableSLRUCache<K, V>(a: Int, b: Int) : SLRUCache<K, V>(a, b) {
+
+    private val delayedPurgeQueue = ContainerUtil.createConcurrentWeakValueMap<K, V>()
+
+    override fun onDropFromCache(key: K, value: V) {
+        delayedPurgeQueue[key] = value
+    }
+
+    override fun get(key: K): V {
+        return delayedPurgeQueue[key] ?: super.get(key)
+    }
+
+    override fun getIfCached(key: K): V? {
+        return delayedPurgeQueue[key] ?: super.getIfCached(key)
+    }
+
+    override fun entrySet(): MutableSet<MutableMap.MutableEntry<K, V>> {
+        error("Unsupported")
+    }
+
+    override fun iterateKeys(keyConsumer: Consumer<K>?) {
+        error("Unsupported")
     }
 }
