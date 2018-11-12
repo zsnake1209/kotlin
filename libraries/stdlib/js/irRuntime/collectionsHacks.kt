81bb3376cf76a734fdf3552df3876b9c39ae5ea3
Roman Artemev <roman.artemev@jetbrains.com>
2018-11-12 14:28:47 +0300

    Refact stdlib generator, add support for different backends
    [JS IR BE] Runtime fixes
     * Do not generate external declarations for IR BE
     * Move `arrayToString` helper function out of shared JS stdlib
     * Fix arrays type check for IR BE

--- libraries/stdlib/js/irRuntime/collectionsHacks.kt
+++ libraries/stdlib/js/irRuntime/collectionsHacks.kt
@@ -5,9 +5,54 @@
 
 package kotlin.collections
 
+import kotlin.js.*
+
 // Copied from libraries/stdlib/js/src/kotlin/collections/utils.kt
 // Current inliner doesn't rename symbols inside `js` fun
 @Suppress("UNUSED_PARAMETER")
 internal fun deleteProperty(obj: Any, property: Any) {
     js("delete obj[property]")
+}
+
+internal fun arrayToString(array: Array<*>) = array.map { toString(it) }.joinToString(", ", "[", "]")
+
+internal infix fun <T> Array<out T>.contentDeepEqualsInternal(other: Array<out T>) = contentDeepEqualsImpl(other)
+
+internal fun <T> Array<out T>.contentDeepHashCodeInternal() = contentDeepHashCodeImpl()
+
+internal fun <T> Array<out T>.contentDeepToStringInternal() = contentDeepToStringImpl()
+
+internal fun <T> T.contentEqualsInternal(other: T): Boolean {
+    val a = this.asDynamic()
+    val b = other.asDynamic()
+
+    if (a === b) return true
+
+    if (!isArrayish(b) || a.length != b.length) return false
+
+    for (i in 0 until a.length) {
+        if (a[i] != b[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+internal fun <T> Array<out T>.contentHashCodeInternal(): Int = fold(1) { a, v -> a * 31 + hashCode(v) }
+
+internal fun <T> T.contentHashCodeInternal(): Int {
+    val a = this.asDynamic()
+    var result = 1
+
+    for (i in 0 until a.length) {
+        result = result * 31 + hashCode(a[i])
+    }
+
+    return result
+}
+
+internal fun <T> T.contentToStringInternal() = arrayToString(this as Array<*>)
+
+internal fun <T> T.primitiveArraySortInternal(): Unit {
+    this.asDynamic().sort()
 }
