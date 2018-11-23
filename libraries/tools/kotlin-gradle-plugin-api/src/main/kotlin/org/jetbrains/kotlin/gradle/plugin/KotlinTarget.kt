c7fae8de6d9c3aac4d4f10705afe3063f5c55c23
Sergey Igushkin <hotkeytlt@gmail.com>
2018-11-23 18:24:30 +0300

    Add a function `attributes { }` to KotlinTarget and KotlinCompilation
    Gradle has a function `attributes { ... }` for `Configuration`, so we
    can introduce one, too, for uniformity of the DSLs. This will configure
    the `val attributes: AttributeContainer` of the `KotlinTarget` and
    `KotlinCompilation`.
    
    Issue #KT-27682 Fixed

--- libraries/tools/kotlin-gradle-plugin-api/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTarget.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTarget.kt
@@ -10,10 +10,12 @@ import org.gradle.api.Action
 import org.gradle.api.Named
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
+import org.gradle.api.attributes.AttributeContainer
 import org.gradle.api.attributes.HasAttributes
 import org.gradle.api.component.SoftwareComponent
 import org.gradle.api.internal.component.UsageContext
 import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.util.ConfigureUtil
 import org.jetbrains.kotlin.gradle.dsl.KotlinCommonToolOptions
 
 interface KotlinTargetComponent : SoftwareComponent {
@@ -45,6 +47,9 @@ interface KotlinTarget : Named, HasAttributes {
 
     fun mavenPublication(action: Closure<Unit>)
     fun mavenPublication(action: Action<MavenPublication>)
+
+    fun attributes(configure: AttributeContainer.() -> Unit) = configure(attributes)
+    fun attributes(configure: Closure<*>) = attributes { ConfigureUtil.configure(configure, this) }
 
     val originatingPreset: KotlinTargetPreset<out KotlinTarget>?
 
