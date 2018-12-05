db3089ec96836c5efd587fbbdc7185eebc6f965c
Dmitry Jemerov <yole@jetbrains.com>
2018-12-05 15:00:53 +0100

    Calculate keyword completion variants before reference variants
    Keywords can be calculated faster, so this should improve completion
    responsiveness.

--- idea/idea-completion/src/org/jetbrains/kotlin/idea/completion/BasicCompletionSession.kt
+++ idea/idea-completion/src/org/jetbrains/kotlin/idea/completion/BasicCompletionSession.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.completion
@@ -195,6 +184,9 @@ class BasicCompletionSession(
         }
 
         override fun doComplete() {
+            KEYWORDS_ONLY.doComplete()
+            flushToResultSet()
+
             val declaration = isStartOfExtensionReceiverFor()
             if (declaration != null) {
                 completeDeclarationNameFromUnresolvedOrOverride(declaration)
@@ -261,8 +253,6 @@ class BasicCompletionSession(
                 }
                 referenceVariantsCollector!!.collectingFinished()
             }
-
-            KEYWORDS_ONLY.doComplete()
 
             // getting root packages from scope is very slow so we do this in alternative way
             if (callTypeAndReceiver.receiver == null && callTypeAndReceiver.callType.descriptorKindFilter.kindMask.and(DescriptorKindFilter.PACKAGES_MASK) != 0) {
@@ -684,6 +674,7 @@ class BasicCompletionSession(
             }
 
             KEYWORDS_ONLY.doComplete()
+            flushToResultSet()
 
             completeDeclarationNameFromUnresolvedOrOverride(declaration)
 
