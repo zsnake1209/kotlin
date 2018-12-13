3833f9fa790a4ae98646b54bdaf77ddc82617568
Pavel V. Talanov <pavel.talanov@jetbrains.com>
2018-12-13 17:34:13 +0100

    ~~~ NEED TESTS for this

--- idea/idea-android/src/org/jetbrains/kotlin/android/run/KotlinAndroidJUnitRunConfigurationProducer.kt
+++ idea/idea-android/src/org/jetbrains/kotlin/android/run/KotlinAndroidJUnitRunConfigurationProducer.kt
@@ -10,14 +10,19 @@ import com.android.tools.idea.testartifacts.junit.AndroidJUnitConfigurationType
 import com.android.tools.idea.testartifacts.junit.AndroidJUnitConfigurations
 import com.intellij.execution.actions.ConfigurationFromContext
 import org.jetbrains.kotlin.idea.run.AbstractKotlinJUnitRunConfigurationProducer
+import org.jetbrains.kotlin.idea.run.KotlinJUnitRunConfigurationProducer
 
 class KotlinAndroidJUnitRunConfigurationProducer :
     AbstractKotlinJUnitRunConfigurationProducer<AndroidJUnitConfiguration>(AndroidJUnitConfigurationType.getInstance()) {
     override fun shouldReplace(self: ConfigurationFromContext, other: ConfigurationFromContext): Boolean {
+        if (other.isProducedBy(KotlinJUnitRunConfigurationProducer::class.java)) return true
+
         return super.shouldReplace(self, other) && AndroidJUnitConfigurations.shouldUseAndroidJUnitConfigurations(self, other)
     }
 
     override fun isPreferredConfiguration(self: ConfigurationFromContext, other: ConfigurationFromContext): Boolean {
+        if (other.isProducedBy(KotlinJUnitRunConfigurationProducer::class.java)) return true
+
         return super.isPreferredConfiguration(self, other) && AndroidJUnitConfigurations.shouldUseAndroidJUnitConfigurations(self, other)
     }
 }
