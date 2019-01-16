59b6d72fd64f97e88929ebd8c7293f1ea5e992da
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-01-16 21:10:57 +0300

    Fix fake inliner variables spilling
    While generating state machine, we check whether the variable is fake by
    checking the range of the variable BEFORE suspension point.
    However, in some cases the variable ends BEFORE suspension point and
    continues AFTER it. In this case, it is more safe to check the range
    at the end of the suspension point.
     #KT-29317 Fixed

--- compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/CoroutineTransformerMethodVisitor.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/CoroutineTransformerMethodVisitor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the license/LICENSE.txt file.
  */
 
@@ -546,7 +546,7 @@ class CoroutineTransformerMethodVisitor(
             val variablesToSpill =
                 (0 until localsCount)
                     .filterNot {
-                        it in setOf(continuationIndex, dataIndex, exceptionIndex) || it.isInlinerFakeVariable(suspensionCallBegin.index())
+                        it in setOf(continuationIndex, dataIndex, exceptionIndex) || it.isInlinerFakeVariable(suspension.suspensionCallEnd.index())
                     }.map { Pair(it, frame.getLocal(it)) }
                     .filter { (index, value) ->
                         (index == 0 && needDispatchReceiver && isForNamedFunction) ||
