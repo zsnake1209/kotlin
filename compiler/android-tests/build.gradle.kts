74d95d639565c70c8be95bf8bb67f7cdce97f669
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-01-25 14:00:56 +0100

    Add system property to filter android test execution

--- compiler/android-tests/build.gradle.kts
+++ compiler/android-tests/build.gradle.kts
@@ -44,5 +44,7 @@ projectTest {
         systemProperty("kotlin.test.android.teamcity", true)
     }
 
+    systemProperty("kotlin.test.android.path.filter", System.getProperty("kotlin.test.android.path.filter"))
+
     workingDir = rootDir
 }
