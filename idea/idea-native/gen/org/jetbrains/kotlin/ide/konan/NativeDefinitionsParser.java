4401405be8d47e39f1ff834c7c5749bf5e5acc27
kirill.shmakov <kirill.shmakov@jetbrains.com>
2019-01-29 14:18:41 +0300

    Better platforms.

--- idea/idea-native/gen/org/jetbrains/kotlin/ide/konan/NativeDefinitionsParser.java
+++ idea/idea-native/gen/org/jetbrains/kotlin/ide/konan/NativeDefinitionsParser.java
@@ -1,8 +1,3 @@
-/*
- * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
- * that can be found in the license/LICENSE.txt file.
- */
-
 // This is a generated file. Not intended for manual editing.
 package org.jetbrains.kotlin.ide.konan;
 
@@ -28,8 +23,26 @@ public class NativeDefinitionsParser implements PsiParser, LightPsiParser {
     boolean r;
     b = adapt_builder_(t, b, this, null);
     Marker m = enter_section_(b, 0, _COLLAPSE_, null);
-    if (t == CODE) {
-      r = CODE(b, 0);
+    if (t == BOOL_KEY) {
+      r = boolKey(b, 0);
+    }
+    else if (t == BOOL_VALUE) {
+      r = boolValue(b, 0);
+    }
+    else if (t == CODE) {
+      r = code(b, 0);
+    }
+    else if (t == STRING_KEY) {
+      r = stringKey(b, 0);
+    }
+    else if (t == STRING_VALUE) {
+      r = stringValue(b, 0);
+    }
+    else if (t == STRINGS_KEY) {
+      r = stringsKey(b, 0);
+    }
+    else if (t == STRINGS_VALUE) {
+      r = stringsValue(b, 0);
     }
     else {
       r = parse_root_(t, b, 0);
@@ -38,13 +51,59 @@ public class NativeDefinitionsParser implements PsiParser, LightPsiParser {
   }
 
   protected boolean parse_root_(IElementType t, PsiBuilder b, int l) {
-    return ROOT(b, l + 1);
+    return root(b, l + 1);
+  }
+
+  /* ********************************************************** */
+  // boolKey platform_? SEPARATOR boolValue
+  static boolean boolDefinition_(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "boolDefinition_")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = boolKey(b, l + 1);
+    r = r && boolDefinition__1(b, l + 1);
+    r = r && consumeToken(b, SEPARATOR);
+    r = r && boolValue(b, l + 1);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  // platform_?
+  private static boolean boolDefinition__1(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "boolDefinition__1")) return false;
+    platform_(b, l + 1);
+    return true;
+  }
+
+  /* ********************************************************** */
+  // DISABLE_DESIGNATED_INITIALIZER_CHECKS | EXCLUDE_DEPENDENT_MODULES | EXCLUDE_SYSTEM_LIBS
+  public static boolean boolKey(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "boolKey")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NONE_, BOOL_KEY, "<bool key>");
+    r = consumeToken(b, DISABLE_DESIGNATED_INITIALIZER_CHECKS);
+    if (!r) r = consumeToken(b, EXCLUDE_DEPENDENT_MODULES);
+    if (!r) r = consumeToken(b, EXCLUDE_SYSTEM_LIBS);
+    exit_section_(b, l, m, r, false, null);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // VALUE
+  public static boolean boolValue(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "boolValue")) return false;
+    if (!nextTokenIs(b, VALUE)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeToken(b, VALUE);
+    exit_section_(b, m, BOOL_VALUE, r);
+    return r;
   }
 
   /* ********************************************************** */
   // CODE_CHARS
-  public static boolean CODE(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "CODE")) return false;
+  public static boolean code(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "code")) return false;
     if (!nextTokenIs(b, CODE_CHARS)) return false;
     boolean r;
     Marker m = enter_section_(b);
@@ -54,97 +113,218 @@ public class NativeDefinitionsParser implements PsiParser, LightPsiParser {
   }
 
   /* ********************************************************** */
-  // definitions_ (DELIM CODE)?
-  static boolean ROOT(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "ROOT")) return false;
+  // COMMENT | boolDefinition_ | stringDefinition_ | stringsDefinition_
+  static boolean definitionItem_(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "definitionItem_")) return false;
     boolean r;
-    Marker m = enter_section_(b);
-    r = definitions_(b, l + 1);
-    r = r && ROOT_1(b, l + 1);
-    exit_section_(b, m, null, r);
-    return r;
-  }
-
-  // (DELIM CODE)?
-  private static boolean ROOT_1(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "ROOT_1")) return false;
-    ROOT_1_0(b, l + 1);
-    return true;
-  }
-
-  // DELIM CODE
-  private static boolean ROOT_1_0(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "ROOT_1_0")) return false;
-    boolean r;
-    Marker m = enter_section_(b);
-    r = consumeToken(b, DELIM);
-    r = r && CODE(b, l + 1);
-    exit_section_(b, m, null, r);
+    r = consumeToken(b, COMMENT);
+    if (!r) r = boolDefinition_(b, l + 1);
+    if (!r) r = stringDefinition_(b, l + 1);
+    if (!r) r = stringsDefinition_(b, l + 1);
     return r;
   }
 
   /* ********************************************************** */
-  // key_ SEPARATOR VALUE
-  static boolean definition_(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "definition_")) return false;
-    if (!nextTokenIs(b, "", KEY_KNOWN, KEY_UNKNOWN)) return false;
+  // LANGUAGE | LINKER
+  static boolean extensibleStringKey_(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "extensibleStringKey_")) return false;
+    if (!nextTokenIs(b, "", LANGUAGE, LINKER)) return false;
+    boolean r;
+    r = consumeToken(b, LANGUAGE);
+    if (!r) r = consumeToken(b, LINKER);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // ANDROID | ANDROID_ARM32 | ANDROID_ARM64 | ARM32 | ARM64 | IOS | IOS_ARM32
+  //                         | IOS_ARM64 | IOS_X64 | LINUX | LINUX_ARM32_HFP | LINUX_MIPS32
+  //                         | LINUX_MIPSEL32 | LINUX_X64 | MACOS_X64 | MINGW | MINGW_X64 | MIPS32
+  //                         | MIPSEL32 | OSX | WASM | WASM32 | X64 | UNKNOWN_PLATFORM
+  static boolean platform_(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "platform_")) return false;
+    boolean r;
+    r = consumeToken(b, ANDROID);
+    if (!r) r = consumeToken(b, ANDROID_ARM32);
+    if (!r) r = consumeToken(b, ANDROID_ARM64);
+    if (!r) r = consumeToken(b, ARM32);
+    if (!r) r = consumeToken(b, ARM64);
+    if (!r) r = consumeToken(b, IOS);
+    if (!r) r = consumeToken(b, IOS_ARM32);
+    if (!r) r = consumeToken(b, IOS_ARM64);
+    if (!r) r = consumeToken(b, IOS_X64);
+    if (!r) r = consumeToken(b, LINUX);
+    if (!r) r = consumeToken(b, LINUX_ARM32_HFP);
+    if (!r) r = consumeToken(b, LINUX_MIPS32);
+    if (!r) r = consumeToken(b, LINUX_MIPSEL32);
+    if (!r) r = consumeToken(b, LINUX_X64);
+    if (!r) r = consumeToken(b, MACOS_X64);
+    if (!r) r = consumeToken(b, MINGW);
+    if (!r) r = consumeToken(b, MINGW_X64);
+    if (!r) r = consumeToken(b, MIPS32);
+    if (!r) r = consumeToken(b, MIPSEL32);
+    if (!r) r = consumeToken(b, OSX);
+    if (!r) r = consumeToken(b, WASM);
+    if (!r) r = consumeToken(b, WASM32);
+    if (!r) r = consumeToken(b, X64);
+    if (!r) r = consumeToken(b, UNKNOWN_PLATFORM);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // definitionItem_* (DELIM code)?
+  static boolean root(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "root")) return false;
     boolean r;
     Marker m = enter_section_(b);
-    r = key_(b, l + 1);
-    r = r && consumeTokens(b, 0, SEPARATOR, VALUE);
+    r = root_0(b, l + 1);
+    r = r && root_1(b, l + 1);
     exit_section_(b, m, null, r);
     return r;
   }
 
-  /* ********************************************************** */
-  // definition_ | COMMENT
-  static boolean definition_item_(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "definition_item_")) return false;
-    boolean r;
-    r = definition_(b, l + 1);
-    if (!r) r = consumeToken(b, COMMENT);
-    return r;
-  }
-
-  /* ********************************************************** */
-  // definition_item_*
-  static boolean definitions_(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "definitions_")) return false;
+  // definitionItem_*
+  private static boolean root_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "root_0")) return false;
     while (true) {
       int c = current_position_(b);
-      if (!definition_item_(b, l + 1)) break;
-      if (!empty_element_parsed_guard_(b, "definitions_", c)) break;
+      if (!definitionItem_(b, l + 1)) break;
+      if (!empty_element_parsed_guard_(b, "root_0", c)) break;
     }
     return true;
   }
 
-  /* ********************************************************** */
-  // (KEY_KNOWN | KEY_UNKNOWN) PLATFORM?
-  static boolean key_(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "key_")) return false;
-    if (!nextTokenIs(b, "", KEY_KNOWN, KEY_UNKNOWN)) return false;
+  // (DELIM code)?
+  private static boolean root_1(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "root_1")) return false;
+    root_1_0(b, l + 1);
+    return true;
+  }
+
+  // DELIM code
+  private static boolean root_1_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "root_1_0")) return false;
     boolean r;
     Marker m = enter_section_(b);
-    r = key__0(b, l + 1);
-    r = r && key__1(b, l + 1);
+    r = consumeToken(b, DELIM);
+    r = r && code(b, l + 1);
     exit_section_(b, m, null, r);
     return r;
   }
 
-  // KEY_KNOWN | KEY_UNKNOWN
-  private static boolean key__0(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "key__0")) return false;
+  /* ********************************************************** */
+  // stringKey SEPARATOR stringValue
+  static boolean stringDefinition_(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringDefinition_")) return false;
     boolean r;
-    r = consumeToken(b, KEY_KNOWN);
-    if (!r) r = consumeToken(b, KEY_UNKNOWN);
+    Marker m = enter_section_(b);
+    r = stringKey(b, l + 1);
+    r = r && consumeToken(b, SEPARATOR);
+    r = r && stringValue(b, l + 1);
+    exit_section_(b, m, null, r);
     return r;
   }
 
-  // PLATFORM?
-  private static boolean key__1(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "key__1")) return false;
-    consumeToken(b, PLATFORM);
+  /* ********************************************************** */
+  // extensibleStringKey_ platform_? | PACKAGE
+  public static boolean stringKey(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringKey")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NONE_, STRING_KEY, "<string key>");
+    r = stringKey_0(b, l + 1);
+    if (!r) r = consumeToken(b, PACKAGE);
+    exit_section_(b, l, m, r, false, null);
+    return r;
+  }
+
+  // extensibleStringKey_ platform_?
+  private static boolean stringKey_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringKey_0")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = extensibleStringKey_(b, l + 1);
+    r = r && stringKey_0_1(b, l + 1);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  // platform_?
+  private static boolean stringKey_0_1(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringKey_0_1")) return false;
+    platform_(b, l + 1);
     return true;
+  }
+
+  /* ********************************************************** */
+  // VALUE
+  public static boolean stringValue(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringValue")) return false;
+    if (!nextTokenIs(b, VALUE)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeToken(b, VALUE);
+    exit_section_(b, m, STRING_VALUE, r);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // stringsKey platform_? SEPARATOR stringsValue
+  static boolean stringsDefinition_(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringsDefinition_")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = stringsKey(b, l + 1);
+    r = r && stringsDefinition__1(b, l + 1);
+    r = r && consumeToken(b, SEPARATOR);
+    r = r && stringsValue(b, l + 1);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  // platform_?
+  private static boolean stringsDefinition__1(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringsDefinition__1")) return false;
+    platform_(b, l + 1);
+    return true;
+  }
+
+  /* ********************************************************** */
+  // COMPILER_OPTS | DEPENDS  | ENTRY_POINT | EXCLUDED_FUNCTIONS
+  //                             | EXCLUDED_MACROS  | EXPORT_FORWARD_DECLARATIONS | HEADER_FILTER
+  //                             | HEADERS | LIBRARY_PATHS | LINKER_OPTS | NON_STRICT_ENUMS
+  //                             | NO_STRING_CONVERSION | PACKAGE | STATIC_LIBRARIES | STRICT_ENUMS
+  public static boolean stringsKey(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringsKey")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NONE_, STRINGS_KEY, "<strings key>");
+    r = consumeToken(b, COMPILER_OPTS);
+    if (!r) r = consumeToken(b, DEPENDS);
+    if (!r) r = consumeToken(b, ENTRY_POINT);
+    if (!r) r = consumeToken(b, EXCLUDED_FUNCTIONS);
+    if (!r) r = consumeToken(b, EXCLUDED_MACROS);
+    if (!r) r = consumeToken(b, EXPORT_FORWARD_DECLARATIONS);
+    if (!r) r = consumeToken(b, HEADER_FILTER);
+    if (!r) r = consumeToken(b, HEADERS);
+    if (!r) r = consumeToken(b, LIBRARY_PATHS);
+    if (!r) r = consumeToken(b, LINKER_OPTS);
+    if (!r) r = consumeToken(b, NON_STRICT_ENUMS);
+    if (!r) r = consumeToken(b, NO_STRING_CONVERSION);
+    if (!r) r = consumeToken(b, PACKAGE);
+    if (!r) r = consumeToken(b, STATIC_LIBRARIES);
+    if (!r) r = consumeToken(b, STRICT_ENUMS);
+    exit_section_(b, l, m, r, false, null);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // VALUE
+  public static boolean stringsValue(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "stringsValue")) return false;
+    if (!nextTokenIs(b, VALUE)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeToken(b, VALUE);
+    exit_section_(b, m, STRINGS_VALUE, r);
+    return r;
   }
 
 }
--- idea/idea-native/src/org/jetbrains/kotlin/ide/konan/NativeDefinitions.kt
+++ idea/idea-native/src/org/jetbrains/kotlin/ide/konan/NativeDefinitions.kt
@@ -99,10 +99,55 @@ class NativeDefinitionsSyntaxHighlighter : SyntaxHighlighterBase() {
             TokenType.BAD_CHARACTER -> BAD_CHAR_KEYS
             NativeDefinitionsTypes.COMMENT -> COMMENT_KEYS
             NativeDefinitionsTypes.DELIM -> COMMENT_KEYS
-            NativeDefinitionsTypes.KEY_KNOWN -> KEYWORD_KEYS
-            NativeDefinitionsTypes.KEY_UNKNOWN -> BAD_CHAR_KEYS
             NativeDefinitionsTypes.SEPARATOR -> OPERATOR_KEYS
+            NativeDefinitionsTypes.UNKNOWN_KEY -> BAD_CHAR_KEYS
+            NativeDefinitionsTypes.UNKNOWN_PLATFORM -> BAD_CHAR_KEYS
             NativeDefinitionsTypes.VALUE -> VALUE_KEYS
+            // known properties
+            NativeDefinitionsTypes.COMPILER_OPTS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.DEPENDS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.DISABLE_DESIGNATED_INITIALIZER_CHECKS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.ENTRY_POINT -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.EXCLUDE_DEPENDENT_MODULES -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.EXCLUDE_SYSTEM_LIBS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.EXCLUDED_FUNCTIONS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.EXCLUDED_MACROS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.EXPORT_FORWARD_DECLARATIONS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.HEADERS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.HEADER_FILTER -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.LANGUAGE -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.LIBRARY_PATHS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.LINKER -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.LINKER_OPTS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.NON_STRICT_ENUMS -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.NO_STRING_CONVERSION -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.PACKAGE -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.STATIC_LIBRARIES -> KNOWN_PROPERTIES_KEYS
+            NativeDefinitionsTypes.STRICT_ENUMS -> KNOWN_PROPERTIES_KEYS
+            // known extensions
+            NativeDefinitionsTypes.ANDROID -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.ANDROID_ARM32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.ANDROID_ARM64 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.ARM32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.ARM64 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.IOS -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.IOS_ARM32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.IOS_ARM64 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.IOS_X64 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.LINUX -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.LINUX_ARM32_HFP -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.LINUX_MIPS32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.LINUX_MIPSEL32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.LINUX_X64 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.MACOS_X64 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.MINGW -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.MINGW_X64 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.MIPS32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.MIPSEL32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.OSX -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.WASM -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.WASM32 -> KNOWN_EXTENSIONS_KEYS
+            NativeDefinitionsTypes.X64 -> KNOWN_EXTENSIONS_KEYS
             else -> EMPTY_KEYS
         }
 
@@ -116,7 +161,8 @@ class NativeDefinitionsSyntaxHighlighter : SyntaxHighlighterBase() {
         val BAD_CHAR_KEYS = createKeys("Unknown key", HighlighterColors.BAD_CHARACTER)
         val COMMENT_KEYS = createKeys("Comment", DefaultLanguageHighlighterColors.LINE_COMMENT)
         val EMPTY_KEYS = emptyArray<TextAttributesKey>()
-        val KEYWORD_KEYS = createKeys("Known key", DefaultLanguageHighlighterColors.KEYWORD)
+        val KNOWN_EXTENSIONS_KEYS = createKeys("Known extension", DefaultLanguageHighlighterColors.LABEL)
+        val KNOWN_PROPERTIES_KEYS = createKeys("Known property", DefaultLanguageHighlighterColors.KEYWORD)
         val OPERATOR_KEYS = createKeys("Operator", DefaultLanguageHighlighterColors.OPERATION_SIGN)
         val VALUE_KEYS = createKeys("Value", DefaultLanguageHighlighterColors.STRING)
     }
