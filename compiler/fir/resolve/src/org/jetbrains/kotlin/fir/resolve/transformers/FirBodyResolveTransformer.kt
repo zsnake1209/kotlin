26252883c15d8ff35c6d66265c3accd7c23b664e
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-03-14 17:52:44 +0300

    FIR: Support typing of type operators

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirBodyResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirBodyResolveTransformer.kt
@@ -88,6 +88,30 @@ open class FirBodyResolveTransformer(val session: FirSession, val implicitTypeOn
         }
     }
 
+    override fun transformTypeOperatorCall(typeOperatorCall: FirTypeOperatorCall, data: Any?): CompositeTransformResult<FirStatement> {
+        val symbolProvider = session.service<FirSymbolProvider>()
+        val resolved = super.transformTypeOperatorCall(typeOperatorCall, data).single
+        when ((resolved as FirTypeOperatorCall).operation) {
+            FirOperation.IS, FirOperation.NOT_IS -> {
+                bindingContext[resolved] = FirResolvedTypeRefImpl(
+                    session,
+                    null,
+                    StandardClassIds.Boolean(symbolProvider).constructType(emptyArray(), isNullable = false),
+                    false,
+                    emptyList()
+                )
+            }
+            FirOperation.AS -> {
+                bindingContext[resolved] = resolved.typeRef
+            }
+            FirOperation.SAFE_AS -> {
+                bindingContext[resolved] =
+                        resolved.typeRef.withReplacedConeType(resolved.typeRef.coneTypeUnsafe().withNullability(ConeNullability.NULLABLE))
+            }
+            else -> error("Unknown type operator")
+        }
+        return resolved.compose()
+    }
 
     protected inline fun <T> withScopeCleanup(scopes: MutableList<*>, crossinline l: () -> T): T {
         val sizeBefore = scopes.size
