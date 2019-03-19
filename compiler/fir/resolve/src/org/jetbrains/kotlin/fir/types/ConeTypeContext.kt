5c45457eb2c1fd804d7cb85daf7df3d077c09273
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-03-19 16:49:37 +0300

    Expand ConeTypeContext

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/types/ConeTypeContext.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/types/ConeTypeContext.kt
@@ -338,22 +338,6 @@ interface ConeTypeContext : TypeSystemContext, TypeSystemOptimizationContext {
                 symbol is FirTypeParameterSymbol
     }
 
-    override fun intersectTypes(types: List<KotlinTypeMarker>): KotlinTypeMarker {
-        TODO("not implemented")
-    }
-
-    override fun intersectTypes(types: List<SimpleTypeMarker>): SimpleTypeMarker {
-        TODO("not implemented")
-    }
-
-    override fun prepareType(type: KotlinTypeMarker): KotlinTypeMarker {
-        TODO("not implemented")
-    }
-
-    override fun SimpleTypeMarker.isStubType(): Boolean {
-        TODO("not implemented")
-    }
-
     override fun captureFromExpression(type: KotlinTypeMarker): KotlinTypeMarker? {
         TODO("not implemented")
     }
@@ -362,6 +346,21 @@ interface ConeTypeContext : TypeSystemContext, TypeSystemOptimizationContext {
         TODO("not implemented")
     }
 
+    override fun SimpleTypeMarker.isStubType(): Boolean {
+        return false // TODO
+    }
+
+    override fun intersectTypes(types: List<SimpleTypeMarker>): SimpleTypeMarker {
+        return types.first() // TODO: proper implementation
+    }
+
+    override fun intersectTypes(types: List<KotlinTypeMarker>): KotlinTypeMarker {
+        return types.first() // TODO: proper implementation
+    }
+
+    override fun prepareType(type: KotlinTypeMarker): KotlinTypeMarker {
+        return type
+    }
 }
 
 class ConeTypeCheckerContext(override val isErrorTypeEqualsToAnything: Boolean, override val session: FirSession) :
@@ -379,16 +378,11 @@ class ConeTypeCheckerContext(override val isErrorTypeEqualsToAnything: Boolean, 
         return a == b
     }
 
-    override fun intersectTypes(types: List<KotlinTypeMarker>): KotlinTypeMarker {
-        return types.first() // TODO: proper implementation
+    override fun prepareType(type: KotlinTypeMarker): KotlinTypeMarker {
+        return super<ConeTypeContext>.prepareType(type)
     }
 
     override val KotlinTypeMarker.isAllowedTypeVariable: Boolean
         get() = false
-
-
-    override fun prepareType(type: KotlinTypeMarker): KotlinTypeMarker {
-        return super<ConeTypeContext>.prepareType(type)
-    }
 
 }
