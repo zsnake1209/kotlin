3c4766973ae318c723c178fe8f2d653f5554b55b
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-03-20 22:07:29 +0300

    FIR: Stub for super references

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirBodyResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirBodyResolveTransformer.kt
@@ -166,6 +166,11 @@ open class FirBodyResolveTransformer(val session: FirSession, val implicitTypeOn
                 val type = types.lastOrNull() ?: ConeKotlinErrorType("Unresolved this@$labelName")
                 bindingContext[qualifiedAccessExpression] = FirResolvedTypeRefImpl(session, null, type, false, emptyList())
             }
+            is FirSuperReference -> {
+                qualifiedAccessExpression.resultType =
+                    FirErrorTypeRefImpl(session, qualifiedAccessExpression.psi, "Unsupported: super type") //TODO
+
+            }
         }
         val callee = qualifiedAccessExpression.calleeReference as? FirSimpleNamedReference ?: return qualifiedAccessExpression.compose()
 
