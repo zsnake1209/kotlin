49658c9a2664f04c72db3600572af487c63ca363
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2019-03-29 16:18:22 +0300

    ~ review fix: removed some unnecessary dependencies

--- compiler/ir/serialization.js/build.gradle.kts
+++ compiler/ir/serialization.js/build.gradle.kts
@@ -4,14 +4,8 @@ plugins {
 }
 
 dependencies {
-    compile(project(":compiler:util"))
-    compile(project(":compiler:frontend"))
-    compile(project(":compiler:backend-common"))
-    compile(project(":compiler:ir.tree"))
     compile(project(":compiler:ir.psi2ir"))
-    compile(project(":compiler:ir.backend.common"))
     compile(project(":compiler:ir.serialization.common"))
-    compile(project(":js:js.ast"))
     compile(project(":js:js.frontend"))
 
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
