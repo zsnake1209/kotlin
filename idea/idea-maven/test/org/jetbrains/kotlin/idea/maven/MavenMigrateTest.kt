1889035094ae00afde83ed0c055a9e927002750a
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-04-12 12:16:41 +0200

    ~edt

--- idea/idea-maven/test/org/jetbrains/kotlin/idea/maven/MavenMigrateTest.kt
+++ idea/idea-maven/test/org/jetbrains/kotlin/idea/maven/MavenMigrateTest.kt
@@ -9,12 +9,12 @@ import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiManager
+import com.intellij.testFramework.runInEdtAndWait
 import com.intellij.util.concurrency.FutureResult
 import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.config.LanguageVersion
 import org.jetbrains.kotlin.idea.configuration.KotlinMigrationProjectComponent
 import org.jetbrains.kotlin.idea.configuration.MigrationInfo
-import org.jetbrains.kotlin.test.testFramework.runInEdtAndWait
 import org.junit.Assert
 import java.io.File
 import java.util.concurrent.TimeUnit
