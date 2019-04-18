cbac522fca164cdbb00ae9384b4981b8aa18b8b1
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-04-18 16:00:59 +0200

    Wise folder delete

--- compiler/tests-common/tests/org/jetbrains/kotlin/test/IdeaSystemPropertiesForParallelRunConfigurator.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/test/IdeaSystemPropertiesForParallelRunConfigurator.kt
@@ -7,17 +7,19 @@ package org.jetbrains.kotlin.test
 
 import com.intellij.openapi.application.PathManager.PROPERTY_CONFIG_PATH
 import com.intellij.openapi.application.PathManager.PROPERTY_SYSTEM_PATH
+import com.intellij.openapi.util.ShutDownTracker
 import com.intellij.openapi.util.io.FileUtil
 import java.io.File
 
 // It's important that this is not created per test, but rather per process.
 object IdeaSystemPropertiesForParallelRunConfigurator {
     private val GRADLE_WORKER = System.getProperty("org.gradle.test.worker") ?: ""
-    //TODO: try to remove folder on jvm shutdown (there are some flashing test with deleteOnExit = true)
+    private val SYSTEM_TMP = System.getProperty("java.io.tmpdir")
     private val PROCESS_TMP_ROOT_FOLDER =
-        FileUtil.createTempDirectory(File(System.getProperty("java.io.tmpdir")), "testRoot", "GW$GRADLE_WORKER", false).path
-    private val IDEA_SYSTEM = FileUtil.createTempDirectory(File(PROCESS_TMP_ROOT_FOLDER), "idea-system", "", false).path
-    private val IDEA_CONFIG = FileUtil.createTempDirectory(File(PROCESS_TMP_ROOT_FOLDER), "idea-config", "", false).path
+        FileUtil.createTempDirectory(File(SYSTEM_TMP), "testRoot", "GW$GRADLE_WORKER", true).path
+
+    private val IDEA_SYSTEM = FileUtil.createTempDirectory(File(SYSTEM_TMP), "idea-system", "GW$GRADLE_WORKER", false).path
+    private val IDEA_CONFIG = FileUtil.createTempDirectory(File(SYSTEM_TMP), "idea-config", "GW$GRADLE_WORKER", false).path
 
     init {
         // UsefulTestCase temp dir construction could cause folder clash on parallel test execution:
@@ -27,10 +29,24 @@ object IdeaSystemPropertiesForParallelRunConfigurator {
         System.setProperty("java.io.tmpdir", PROCESS_TMP_ROOT_FOLDER)
         System.setProperty(PROPERTY_SYSTEM_PATH, IDEA_SYSTEM)
         System.setProperty(PROPERTY_CONFIG_PATH, IDEA_CONFIG)
+
+        ShutDownTracker.getInstance()
+        Runtime.getRuntime().addShutdownHook(Shutdown(IDEA_SYSTEM, IDEA_CONFIG))
     }
 
     @JvmStatic
     fun setProperties() {
         //TODO: maybe add check for actual folders
+    }
+
+    class Shutdown(vararg val folders: String) : Thread("Dispose idea test caches") {
+        override fun run() {
+            //if (ShutDownTracker.isShutdownHookRunning()) {
+                ShutDownTracker.getInstance().ensureStopperThreadsFinished()
+            //}
+            folders.forEach {
+                FileUtil.delete(File(it))
+            }
+        }
     }
 }
