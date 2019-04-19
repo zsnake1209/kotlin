436c562c2012839c9f872552b8da7e4bad27637f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-04-19 11:25:24 +0300

    ++ replace FirProvider.getCallableSymbols (fix IDE compilation)

--- idea/src/org/jetbrains/kotlin/idea/fir/FirResolutionApi.kt
+++ idea/src/org/jetbrains/kotlin/idea/fir/FirResolutionApi.kt
@@ -12,22 +12,25 @@ import org.jetbrains.kotlin.fir.declarations.FirTypedDeclaration
 import org.jetbrains.kotlin.fir.java.FirJavaModuleBasedSession
 import org.jetbrains.kotlin.fir.java.FirProjectSessionProvider
 import org.jetbrains.kotlin.fir.resolve.FirProvider
-import org.jetbrains.kotlin.fir.resolve.getCallableSymbols
 import org.jetbrains.kotlin.fir.resolve.impl.FirProviderImpl
 import org.jetbrains.kotlin.fir.resolve.transformers.FirImportResolveTransformer
 import org.jetbrains.kotlin.fir.resolve.transformers.FirStatusResolveTransformer
 import org.jetbrains.kotlin.fir.resolve.transformers.FirTypeResolveTransformer
-import org.jetbrains.kotlin.fir.symbols.CallableId
+import org.jetbrains.kotlin.fir.scopes.ProcessorAction
+import org.jetbrains.kotlin.fir.scopes.impl.FirTopLevelDeclaredMemberScope
+import org.jetbrains.kotlin.fir.symbols.ConeCallableSymbol
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
 import org.jetbrains.kotlin.fir.visitors.FirTransformer
 import org.jetbrains.kotlin.idea.caches.project.ModuleSourceInfo
 import org.jetbrains.kotlin.idea.caches.project.getModuleInfo
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtCallableDeclaration
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 
 enum class FirStage(val index: Int, val stubMode: Boolean) {
@@ -72,16 +75,16 @@ fun KtCallableDeclaration.getOrBuildFir(stage: FirStage = FirStage.DECLARATIONS)
     val packageFqName = file.packageFqName
     val klassFqName = this.containingClassOrObject?.relativeFqName()
     val declName = this.nameAsSafeName
-    val callableId = CallableId(packageFqName, klassFqName, declName)
 
     val firProvider = FirProvider.getInstance(session) as FirProviderImpl
     // TODO: minor file modifications should not force full rebuild (!)
-    val cachedFirFile = firProvider.getFirFilesByPackage(packageFqName).find { it.psi == file }
+    var cachedOrNewFirFile = firProvider.getFirFilesByPackage(packageFqName).find { it.psi == file }
     // TODO: stage of cached FIR should be taken into account (!)
-    if (cachedFirFile == null) {
+    if (cachedOrNewFirFile == null) {
         println("FIR resolution: start transformation of ${file.name}")
         val builder = RawFirBuilder(session, stubMode = stage.stubMode)
         val firFile = builder.buildFirFile(file)
+        cachedOrNewFirFile = firFile
         firProvider.recordFile(firFile)
 
         for (transformer in stage.transformers) {
@@ -89,9 +92,27 @@ fun KtCallableDeclaration.getOrBuildFir(stage: FirStage = FirStage.DECLARATIONS)
         }
     }
 
-    return firProvider.getCallableSymbols(callableId).mapNotNull {
-        (it as? FirBasedSymbol<*>)?.fir as? FirCallableMemberDeclaration
-    }.find { it.psi == this }!!
+    val memberScope =
+        if (klassFqName == null) FirTopLevelDeclaredMemberScope(cachedOrNewFirFile, session)
+        else firProvider.getClassDeclaredMemberScope(ClassId(packageFqName, klassFqName, false))!!
+    var result: FirCallableMemberDeclaration? = null
+    val processor = { symbol: ConeCallableSymbol ->
+        val firSymbol = symbol as? FirBasedSymbol<*>
+        val fir = firSymbol?.fir as? FirCallableMemberDeclaration
+        if (fir?.psi == this) {
+            result = fir
+            ProcessorAction.STOP
+        } else {
+            ProcessorAction.NEXT
+        }
+    }
+    if (this is KtNamedFunction) {
+        memberScope.processFunctionsByName(declName, processor)
+    } else {
+        memberScope.processPropertiesByName(declName, processor)
+    }
+
+    return result!!
 }
 
 val FirTypedDeclaration.coneTypeSafe: ConeKotlinType? get() = (this.returnTypeRef as? FirResolvedTypeRef)?.type
