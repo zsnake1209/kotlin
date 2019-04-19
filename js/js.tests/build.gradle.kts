4662214f361fb22e8db52e254b078fa90006641e
Roman Artemev <roman.artemev@jetbrains.com>
2019-04-19 17:53:23 +0300

    implement benchmark draft (WIP)

--- js/js.tests/build.gradle.kts
+++ js/js.tests/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("jvm")
     id("jps-compatible")
     id("com.moowork.node").version("1.2.0")
+    id("me.champeau.gradle.jmh").version("0.4.8")
     id("de.undercouch.download")
 }
 
