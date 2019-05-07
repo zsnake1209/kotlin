81bb9b8017fa3cb0b66806be6f9cc2d5c4792cfa
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-05-08 02:19:35 +0300

    Fix Java interop of inline suspend functions with suspend parameters
    In 1.3.31 I fixed Java interop for inline function with coroutines
    (TL;DR: when we need a state machine, generate two methods: one with
    normal name, and the other one with $$forInline suffix, for the inliner
    to use, just like inline suspend functions), however, I forgot a case
    with inline suspend function with inline suspend function parameter.
    In this case, the compiler a generated two functions, as needed, but,
    neither of them had a state-machine. This change adds the state-machine
    for the method with normal name. Note, that suspend inline functions
    with crossinline parameter, which are also supported by the change,
    did not cause incorrect behaviour, since until now they were generated
    as synthetic.
    
     #KT-31354 Fixed

--- compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/SuspendInlineFunctionGenerationStrategy.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/SuspendInlineFunctionGenerationStrategy.kt
@@ -5,20 +5,28 @@
 
 package org.jetbrains.kotlin.codegen.coroutines
 
+import org.jetbrains.kotlin.builtins.isSuspendFunctionTypeOrSubtype
 import org.jetbrains.kotlin.codegen.ExpressionCodegen
 import org.jetbrains.kotlin.codegen.FunctionCodegen
 import org.jetbrains.kotlin.codegen.FunctionGenerationStrategy
 import org.jetbrains.kotlin.codegen.TransformationMethodVisitor
 import org.jetbrains.kotlin.codegen.inline.coroutines.FOR_INLINE_SUFFIX
+import org.jetbrains.kotlin.codegen.inline.coroutines.findReceiverOfInvoke
+import org.jetbrains.kotlin.codegen.inline.coroutines.surroundInvokesWithSuspendMarkers
+import org.jetbrains.kotlin.codegen.inline.isInvokeOnLambda
+import org.jetbrains.kotlin.codegen.optimization.common.asSequence
+import org.jetbrains.kotlin.codegen.optimization.transformer.MethodTransformer
 import org.jetbrains.kotlin.codegen.state.GenerationState
 import org.jetbrains.kotlin.config.JVMConstructorCallNormalizationMode
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
 import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
 import org.jetbrains.kotlin.resolve.jvm.jvmSignature.JvmMethodSignature
 import org.jetbrains.org.objectweb.asm.MethodVisitor
 import org.jetbrains.org.objectweb.asm.Opcodes
-import org.jetbrains.org.objectweb.asm.tree.MethodNode
+import org.jetbrains.org.objectweb.asm.tree.*
+import org.jetbrains.org.objectweb.asm.tree.analysis.SourceInterpreter
 
 // For named suspend function we generate two methods:
 // 1) to use as noinline function, which have state machine
@@ -44,7 +52,10 @@ class SuspendInlineFunctionGenerationStrategy(
         if (access and Opcodes.ACC_ABSTRACT != 0) return mv
 
         return MethodNodeCopyingMethodVisitor(
-            super.wrapMethodVisitor(mv, access, name, desc), access, name, desc,
+            SurroundSuspendParameterCallsWithSuspendMarkersMethodVisitor(
+                createStateMachineBuilder(mv, access, name, desc),
+                access, name, desc, containingClassInternalName, originalSuspendDescriptor.valueParameters
+            ), access, name, desc,
             newMethod = { origin, newAccess, newName, newDesc ->
                 functionCodegen.newMethod(origin, newAccess, newName, newDesc, null, null)
             }, keepAccess = false
@@ -76,5 +87,36 @@ class MethodNodeCopyingMethodVisitor(
         private fun calculateAccessForInline(access: Int, keepAccess: Boolean): Int =
             if (keepAccess) access
             else access or Opcodes.ACC_PRIVATE and Opcodes.ACC_PUBLIC.inv() and Opcodes.ACC_PROTECTED.inv()
+    }
+}
+
+private class SurroundSuspendParameterCallsWithSuspendMarkersMethodVisitor(
+    delegate: MethodVisitor,
+    access: Int,
+    name: String,
+    desc: String,
+    private val thisName: String,
+    private val valueParameters: List<ValueParameterDescriptor>
+): TransformationMethodVisitor(delegate, access, name, desc, null, null) {
+    override fun performTransformations(methodNode: MethodNode) {
+        fun AbstractInsnNode.index() = methodNode.instructions.indexOf(this)
+        fun AbstractInsnNode.isSuspendParameter() = opcode == Opcodes.ALOAD &&
+                (this as VarInsnNode).`var` - (if (methodNode.access and Opcodes.ACC_STATIC != 0) 0 else 1) < valueParameters.size &&
+                valueParameters[`var`].type.isSuspendFunctionTypeOrSubtype
+
+        val sourceFrames = MethodTransformer.analyze(thisName, methodNode, SourceInterpreter())
+
+        val noinlineInvokes = arrayListOf<Pair<AbstractInsnNode, AbstractInsnNode>>()
+
+        for (insn in methodNode.instructions.asSequence()) {
+            if (insn.opcode != Opcodes.INVOKEINTERFACE) continue
+            insn as MethodInsnNode
+            if (!isInvokeOnLambda(insn.owner, insn.name)) continue
+            val frame = sourceFrames[insn.index()] ?: continue
+            val aload = findReceiverOfInvoke(frame, insn).takeIf { it?.isSuspendParameter() == true } as? VarInsnNode ?: continue
+            noinlineInvokes.add(insn to aload)
+        }
+
+        surroundInvokesWithSuspendMarkers(methodNode, noinlineInvokes)
     }
 }
