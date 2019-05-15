4eb4b072d02c42e8058c1e2962b63ad255768e7b
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-05-15 11:59:29 +0200

    Remove hack with targets. Add target for most recent jdk

--- compiler/tests-different-jdk/tests/org/jetbrains/kotlin/codegen/jdk/CustomJvmTargetOnJvmBaseTest.kt
+++ compiler/tests-different-jdk/tests/org/jetbrains/kotlin/codegen/jdk/CustomJvmTargetOnJvmBaseTest.kt
@@ -95,3 +95,6 @@ class JvmTarget6OnJvmLast : CustomJvmTargetOnJvmBaseTest()
 
 @RunWith(SuiteRunnerForCustomJdk::class)
 class JvmTarget8OnJvmLast : CustomJvmTargetOnJvmBaseTest()
+
+@RunWith(SuiteRunnerForCustomJdk::class)
+class JvmTargetLastOnJvmLast : CustomJvmTargetOnJvmBaseTest()
