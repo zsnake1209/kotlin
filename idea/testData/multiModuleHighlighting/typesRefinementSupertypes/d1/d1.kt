a8e8401a8444e3547f1c687a3a5512cddf6268d7
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-05-15 12:03:01 +0300

    Fix test data for typesRefinementSupertypes
    It's necessary because member scopes are only refined for expect classes

--- idea/testData/multiModuleHighlighting/typesRefinementSupertypes/d1/d1.kt
+++ idea/testData/multiModuleHighlighting/typesRefinementSupertypes/d1/d1.kt
@@ -1,6 +1,6 @@
 package foo
 
-interface Supertype
+<error descr="[UNSUPPORTED_FEATURE] The feature \"multi platform projects\" is experimental and should be enabled explicitly">expect</error> interface Supertype
 
 class A : Supertype
 
--- idea/testData/multiModuleHighlighting/typesRefinementSupertypes/d2/d2.kt
+++ idea/testData/multiModuleHighlighting/typesRefinementSupertypes/d2/d2.kt
@@ -1,7 +1,7 @@
 package foo
 import d0.AnotherSupertype
 
-interface Supertype : AnotherSupertype {
+<error descr="[UNSUPPORTED_FEATURE] The feature \"multi platform projects\" is experimental and should be enabled explicitly">actual</error> interface Supertype : AnotherSupertype {
     fun foo() {}
 }
 
