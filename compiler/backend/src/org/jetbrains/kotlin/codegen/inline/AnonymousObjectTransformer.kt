55fdfa47f5a740d0226056fd420ca58e04c808ce
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-05-20 17:14:54 +0300

    Do not generate $$forInline suffix for enclosing method
    in OUTERCLASS field.
    
    The inliner generates two versions of suspend functions/lambdas in
    inline functions: with state-machine and without. The former is used
    to call the function from Java or via reflection and have ordinary
    name, while the latter is used by inliner and have $$forInline suffix.
    The inliner throws the state-machine version away, duplicates
    $$forInline version and then call state-machine generator.
    
    If these suspend functions/lambdas are not going to be inlined,
    $$forInline version is not generated. However, all objects, which are
    used in these suspend functions/lambdas, have $$forInline version
    written to OUTERCLASS field. This leads to errors by proguard.
    Since they are used in both state-machine version and for-inline ones,
    we can simply remove $$forInline suffix from OUTERCLASS field and this
    fixes the issue.

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/AnonymousObjectTransformer.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/AnonymousObjectTransformer.kt
@@ -240,6 +240,9 @@ class AnonymousObjectTransformer(
 
     private fun writeOuterInfo(visitor: ClassVisitor) {
         val info = inliningContext.callSiteInfo
+        // Since $$forInline functions are not generated if retransformation is the last one (i.e. call site is not inline)
+        // link to the function in OUTERCLASS field becomes invalid. However, since $$forInline function always has no-inline
+        // companion without the suffix, use it.
         if (info.isSuspend && info.isInlineOrInsideInline) {
             visitor.visitOuterClass(info.ownerClassName, info.functionName?.removeSuffix(FOR_INLINE_SUFFIX), info.functionDesc)
         } else {
