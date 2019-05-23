ef8c3acbe90e78902f0ff7d3ccf5fcc1e746c5fc
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2019-05-23 18:31:42 +0300

    Introduce unspecifiedJvmPlatform for clients without jvmTarget
    Previously, a lot of clients used JvmPlatform as platform-marker,
    without thinking about jvmTarget.
    
    For the sake of migration, this commits introduced so-called
    UnspecifiedJvmPlatform, which can be used for a time being, but
    generally, all usages should be removed in  future.

--- compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/AbstractFirDiagnosticsSmokeTest.kt
+++ compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/AbstractFirDiagnosticsSmokeTest.kt
@@ -139,7 +139,7 @@ abstract class AbstractFirDiagnosticsSmokeTest : BaseDiagnosticsTest() {
             when {
                 nameSuffix.isEmpty() -> null // TODO(dsavvinov): this leads to 'null'-platform in ModuleDescriptor
                 nameSuffix == "COMMON" -> CommonPlatforms.defaultCommonPlatform
-                nameSuffix == "JVM" -> JvmPlatforms.defaultJvmPlatform // TODO(dsavvinov): determine JvmTarget precisely
+                nameSuffix == "JVM" -> JvmPlatforms.unspecifiedJvmPlatform // TODO(dsavvinov): determine JvmTarget precisely
                 nameSuffix == "JS" -> JsPlatforms.defaultJsPlatform
                 nameSuffix == "NATIVE" -> KonanPlatforms.defaultKonanPlatform
                 else -> throw IllegalStateException("Can't determine platform by name $nameSuffix")
@@ -149,7 +149,7 @@ abstract class AbstractFirDiagnosticsSmokeTest : BaseDiagnosticsTest() {
 
     class BuiltInModuleInfo(override val name: Name) : ModuleInfo {
         override val platform: TargetPlatform
-            get() = JvmPlatforms.defaultJvmPlatform
+            get() = JvmPlatforms.unspecifiedJvmPlatform
 
         override val analyzerServices: PlatformDependentAnalyzerServices
             get() = JvmPlatformAnalyzerServices
@@ -161,7 +161,7 @@ abstract class AbstractFirDiagnosticsSmokeTest : BaseDiagnosticsTest() {
 
     protected class TestModuleInfo(override val name: Name) : ModuleInfo {
         override val platform: TargetPlatform
-            get() = JvmPlatforms.defaultJvmPlatform
+            get() = JvmPlatforms.unspecifiedJvmPlatform
 
         override val analyzerServices: PlatformDependentAnalyzerServices
             get() = JvmPlatformAnalyzerServices
