d252a7635a5f92088920883d625240c75b04feb8
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-05-30 16:36:49 +0300

    ~~~ Make javac-wrapper a separate module ~~~
    NB: good commit in principle but it's not very necessary

---
+++ compiler/javac-wrapper/build.gradle.kts
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    compile(project(":compiler:util"))
+    compile(project(":compiler:cli-common"))
+    compile(files("${System.getProperty("java.home")}/../lib/tools.jar"))
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
