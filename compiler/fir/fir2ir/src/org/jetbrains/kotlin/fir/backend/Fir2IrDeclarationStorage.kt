bfbf4ddf1d929079488501fe7511ce7a998f9408
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-06-11 17:29:35 +0300

    ++ Temporary FIR2IR fix (caused by 'switch off body visiting' in status)

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrDeclarationStorage.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrDeclarationStorage.kt
@@ -115,7 +115,7 @@ class Fir2IrDeclarationStorage(
         fun create(): IrClass {
             val descriptor = WrappedClassDescriptor()
             val origin = IrDeclarationOrigin.DEFINED
-            val modality = regularClass.modality!!
+            val modality = regularClass.modality ?: Modality.FINAL
             return regularClass.convertWithOffsets { startOffset, endOffset ->
                 irSymbolTable.declareClass(startOffset, endOffset, origin, descriptor, modality) { symbol ->
                     IrClassImpl(
@@ -284,7 +284,7 @@ class Fir2IrDeclarationStorage(
                 irSymbolTable.declareSimpleFunction(startOffset, endOffset, origin, descriptor) { symbol ->
                     IrFunctionImpl(
                         startOffset, endOffset, origin, symbol,
-                        function.name, function.visibility, function.modality!!,
+                        function.name, function.visibility, function.modality ?: Modality.FINAL,
                         function.returnTypeRef.toIrType(session, this),
                         function.isInline, function.isExternal,
                         function.isTailRec, function.isSuspend
@@ -367,7 +367,7 @@ class Fir2IrDeclarationStorage(
                 ) { symbol ->
                     IrPropertyImpl(
                         startOffset, endOffset, origin, symbol,
-                        property.name, property.visibility, property.modality!!,
+                        property.name, property.visibility, property.modality ?: Modality.FINAL,
                         property.isVar, property.isConst, property.isLateInit,
                         property.delegate != null,
                         // TODO
