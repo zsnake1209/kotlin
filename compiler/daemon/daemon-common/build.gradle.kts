000f37eecf4355c4363ae3f481de01abc7c94046
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-06-18 20:05:39 +0300

    ~ move incremental module info

--- compiler/daemon/daemon-common/build.gradle.kts
+++ compiler/daemon/daemon-common/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation(project(":compiler:util"))
     implementation(project(":compiler:cli-common"))
     implementation(project(":compiler:ic:ic-js-base"))
+    implementation(project(":kotlin-build-base"))
     implementation(commonDep("org.jetbrains.kotlinx", "kotlinx-coroutines-core")) {
         isTransitive = false
     }
