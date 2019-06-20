4eb137c9036a3d571f23c84922a7b46d44399a8a
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-06-20 12:48:31 +0300

    FIR: Avoid interface dispatching on FirRegularClass

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/FirClass.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/FirClass.kt
@@ -6,8 +6,11 @@
 package org.jetbrains.kotlin.fir.declarations
 
 import org.jetbrains.kotlin.descriptors.ClassKind
+import org.jetbrains.kotlin.fir.declarations.impl.FirEnumEntryImpl
 import org.jetbrains.kotlin.fir.expressions.FirAnnotationContainer
 import org.jetbrains.kotlin.fir.expressions.FirStatement
+import org.jetbrains.kotlin.fir.symbols.AbstractFirBasedSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.FirTypeRef
 import org.jetbrains.kotlin.fir.types.coneTypeSafe
@@ -39,4 +42,23 @@ interface FirClass : FirDeclarationContainer, FirStatement, FirAnnotationContain
     }
 }
 
-val FirClass.superConeTypes get() = superTypeRefs.mapNotNullTo(ArrayList(superTypeRefs.size)) { it.coneTypeSafe<ConeClassLikeType>() }
+val FirClassSymbol.superConeTypes: List<ConeClassLikeType>
+    get() {
+        // Cast to AbstractFirBasedSymbol<*> is needed to avoid CHECKCAST to FirRegularClass (interface)
+        // that would happen in if it was 'this.fir' while we're trying to avoid interface calls/checkcasts in hot-spots
+        // NB: Looks like JVM doesn't perform devirtualization here because there are more then two implementations of FirRegularClass
+        val fir = (this as AbstractFirBasedSymbol<*>).fir
+
+        // TODO: Remove FirRegularClass as a supertype for enum entry
+        return when (fir) {
+            is FirAbstractRegularClass -> fir.superConeTypes
+            is FirEnumEntryImpl -> fir.superConeTypes
+            else -> error("Unexpected implementation of FirRegularClass: ${fir::class}")
+        }
+    }
+
+private val FirAbstractRegularClass.superConeTypes get() = superTypeRefs.coneTypeSafeRefList()
+private val FirEnumEntryImpl.superConeTypes get() = superTypeRefs.coneTypeSafeRefList()
+
+private fun List<FirTypeRef>.coneTypeSafeRefList(): List<ConeClassLikeType> =
+    mapNotNullTo(ArrayList(size)) { it.coneTypeSafe() }
