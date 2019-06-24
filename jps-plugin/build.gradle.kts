a27b5bfdbf9ef07466a9bd35c02622d509beb1fa
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-06-24 17:36:56 +0300

    Modify dependencies for 192 branch: more dependencies to full java plugin
    (cherry picked from commit b7c12014ee4f01b418e8c9db24181455df534f4c)

--- jps-plugin/build.gradle.kts
+++ jps-plugin/build.gradle.kts
@@ -23,17 +23,7 @@ dependencies {
             includeJars("jdom", "trove4j", "jps-model", "openapi", "util", "asm-all", rootProject = rootProject)
         }
     }
-
-    Platform[191].orLower {
-        testCompileOnly(jpsStandalone()) { includeJars("jps-builders", "jps-builders-6") }
-        compileOnly(jpsStandalone()) { includeJars("jps-builders", "jps-builders-6") }
-    }
-    Platform[192].orHigher {
-        compileOnly(intellijPluginDep("java")) { includeJars("jps-builders", "jps-builders-6") }
-        testCompileOnly(intellijPluginDep("java")) { includeJars("jps-builders", "jps-builders-6") }
-        testRuntimeOnly(intellijPluginDep("java"))
-    }
-
+    compileOnly(jpsStandalone()) { includeJars("jps-builders", "jps-builders-6") }
     testCompileOnly(project(":kotlin-reflect-api"))
     testCompile(project(":compiler:incremental-compilation-impl"))
     testCompile(projectTests(":compiler:tests-common"))
@@ -41,7 +31,7 @@ dependencies {
     testCompile(commonDep("junit:junit"))
     testCompile(project(":kotlin-test:kotlin-test-jvm"))
     testCompile(projectTests(":kotlin-build-common"))
-
+    testCompileOnly(jpsStandalone()) { includeJars("jps-builders", "jps-builders-6") }
     Ide.IJ {
         testCompile(intellijDep("devkit"))
     }
@@ -54,7 +44,11 @@ dependencies {
     compilerModules.forEach {
         testRuntime(project(it))
     }
-    testRuntime(intellijDep())
+
+    Platform[192].orHigher {
+        testRuntimeOnly(intellijPluginDep("java"))
+    }
+
     testRuntime(project(":kotlin-reflect"))
     testRuntime(project(":kotlin-script-runtime"))
 }
