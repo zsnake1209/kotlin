fc37b087b50595836e2cd57267e25c090f8db9c1
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-06-27 18:05:04 +0300

    FIR: Merge FirResolvedFunctionTypeRefImpl into FirResolvedFunctionTypeRef

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/types/FirResolvedTypeRef.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/types/FirResolvedTypeRef.kt
@@ -6,9 +6,10 @@
 package org.jetbrains.kotlin.fir.types
 
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.fir.FirAbstractElement
-import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.VisitedSupertype
+import org.jetbrains.kotlin.fir.*
+import org.jetbrains.kotlin.fir.declarations.FirValueParameter
+import org.jetbrains.kotlin.fir.expressions.FirAnnotationCall
+import org.jetbrains.kotlin.fir.visitors.FirTransformer
 import org.jetbrains.kotlin.fir.visitors.FirVisitor
 
 abstract class FirResolvedTypeRef(
@@ -21,13 +22,27 @@ abstract class FirResolvedTypeRef(
         visitor.visitResolvedTypeRef(this, data)
 }
 
-abstract class FirResolvedFunctionTypeRef(
-    session: FirSession,
+@VisitedClass
+class FirResolvedFunctionTypeRef(
     psi: PsiElement?,
+    session: FirSession,
+    override val isMarkedNullable: Boolean,
+    override val annotations: MutableList<FirAnnotationCall>,
+    override var receiverTypeRef: FirTypeRef?,
+    override val valueParameters: MutableList<FirValueParameter>,
+    override var returnTypeRef: FirTypeRef,
     type: ConeKotlinType
 ) : @VisitedSupertype FirResolvedTypeRef(session, psi, type), FirFunctionTypeRef {
     override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R =
         visitor.visitResolvedFunctionTypeRef(this, data)
+
+    override fun <D> transformChildren(transformer: FirTransformer<D>, data: D): FirElement {
+        receiverTypeRef = receiverTypeRef?.transformSingle(transformer, data)
+        returnTypeRef = returnTypeRef.transformSingle(transformer, data)
+        valueParameters.transformInplace(transformer, data)
+
+        return this
+    }
 }
 
 inline fun <reified T : ConeKotlinType> FirTypeRef.coneTypeUnsafe() = (this as FirResolvedTypeRef).type as T
