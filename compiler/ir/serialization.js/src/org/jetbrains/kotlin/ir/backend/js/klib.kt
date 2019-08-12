0cef87756f104f013723deb06fddf344a988776e
Roman Artemev <roman.artemev@jetbrains.com>
2019-08-12 15:03:21 +0300

    [IR SERIALIZATION] Do not track non-metadata descriptors in descriptor table

--- compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/klib.kt
+++ compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/klib.kt
@@ -297,7 +297,8 @@ fun serializeModuleIntoKlib(
 ) {
     val descriptorTable = DescriptorTable()
 
-    val serializedIr = JsIrModuleSerializer(emptyLoggingContext, moduleFragment.irBuiltins, descriptorTable).serializedIrModule(moduleFragment)
+    val serializedIr =
+        JsIrModuleSerializer(emptyLoggingContext, moduleFragment.irBuiltins, descriptorTable).serializedIrModule(moduleFragment)
     val serializer = JsKlibMetadataSerializationUtil
 
     val moduleDescription =
@@ -308,8 +309,8 @@ fun serializeModuleIntoKlib(
         languageVersionSettings,
         metadataVersion
     ) { declarationDescriptor ->
-        val index = descriptorTable.get(declarationDescriptor)
-        index?.let { newJsDescriptorUniqId(it) }
+        val index = descriptorTable.get(declarationDescriptor) ?: error("No descriptor ID found for $declarationDescriptor")
+        newJsDescriptorUniqId(index)
     }
 
     val abiVersion = KotlinAbiVersion.CURRENT
