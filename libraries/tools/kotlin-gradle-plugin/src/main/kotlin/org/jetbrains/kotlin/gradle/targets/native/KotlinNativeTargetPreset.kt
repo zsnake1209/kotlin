fe6e484189f0682e81f0312dc4ed62dde9cc0d67
Sergey Igushkin <hotkeytlt@gmail.com>
2019-08-13 18:00:36 +0300

    Create objects via Gradle `ObjectFactory` to allow access from Groovy

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/KotlinNativeTargetPreset.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/KotlinNativeTargetPreset.kt
@@ -110,7 +110,9 @@ open class KotlinNativeTargetPreset(name: String, project: Project, konanTarget:
     override fun createTargetConfigurator(): KotlinTargetConfigurator<KotlinNativeTarget> =
         KotlinNativeTargetConfigurator(kotlinPluginVersion)
 
-    override fun instantiateTarget(name: String): KotlinNativeTarget = KotlinNativeTarget(project, konanTarget)
+    override fun instantiateTarget(name: String): KotlinNativeTarget {
+        return project.objects.newInstance(KotlinNativeTarget::class.java, project, konanTarget)
+    }
 }
 
 open class KotlinNativeTargetWithTestsPreset(name: String, project: Project, konanTarget: KonanTarget, kotlinPluginVersion: String) :
@@ -119,7 +121,9 @@ open class KotlinNativeTargetWithTestsPreset(name: String, project: Project, kon
     override fun createTargetConfigurator(): KotlinTargetConfigurator<KotlinNativeTargetWithTests> =
         KotlinNativeTargetWithTestsConfigurator(kotlinPluginVersion)
 
-    override fun instantiateTarget(name: String): KotlinNativeTargetWithTests = KotlinNativeTargetWithTests(project, konanTarget)
+    override fun instantiateTarget(name: String): KotlinNativeTargetWithTests {
+        return project.objects.newInstance(KotlinNativeTargetWithTests::class.java, project, konanTarget)
+    }
 }
 
 internal val KonanTarget.isCurrentHost: Boolean
