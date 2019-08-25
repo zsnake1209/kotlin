430376d88b503c5b0be26e51669f958372480daa
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2019-08-25 16:52:51 +0300

    Fixed testdata related to KT-14781
    #KT-14781 Comment

--- idea/testData/quickfix.special/deprecatedSymbolUsage/defaultParameterValuesFromLibrary.kt
+++ idea/testData/quickfix.special/deprecatedSymbolUsage/defaultParameterValuesFromLibrary.kt
@@ -1,5 +1,9 @@
 // "Replace with 'prefix + joinTo(StringBuilder(), separator, "", postfix, limit, truncated, transform)'" "true"
 
+public fun <T> Iterable<T>.joinToString2(separator: CharSequence = ", ", prefix: CharSequence = "", postfix: CharSequence = "", limit: Int = -1, truncated: CharSequence = "...", transform: ((T) -> CharSequence)? = null): String {
+    return joinTo(StringBuilder(), separator, prefix, postfix, limit, truncated, transform).toString()
+}
+
 fun foo() {
-    listOf(1, 2, 3).<caret>joinToString(limit = 10)
+    listOf(1, 2, 3).<caret>joinToString2(limit = 10)
 }
