6d3d06182f25d4295a32ef7fd374afd4bc90a0a7
Alexey Trilis <aatrilis2000@gmail.com>
2019-09-30 14:55:10 +0300

    Replace idl2k with dukat

---
+++ libraries/tools/dukat/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    maven("https://kotlin.bintray.com/dukat")
+}
+
+dependencies {
+    implementation(kotlinStdlib())
+    implementation("org.jetbrains.dukat:dukat:0.0.17")
+    implementation("org.jsoup:jsoup:1.8.2")
+}
+
+task("downloadIDL", JavaExec::class) {
+    main = "org.jetbrains.kotlin.tools.dukat.DownloadKt"
+    classpath = sourceSets["main"].runtimeClasspath
+    dependsOn(":dukat:build")
+}
+
+task("launchDukat", JavaExec::class) {
+    main = "org.jetbrains.kotlin.tools.dukat.LaunchKt"
+    classpath = sourceSets["main"].runtimeClasspath
+    dependsOn(":dukat:build")
+}
