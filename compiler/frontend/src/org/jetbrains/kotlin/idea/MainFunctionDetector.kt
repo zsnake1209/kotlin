7d2f3840766f445103a0bbbde3387a182731eedb
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2019-10-29 18:30:14 +0100

    Added AllowNullableArrayArgsInMain (1.4+) language setting

--- compiler/frontend/src/org/jetbrains/kotlin/idea/MainFunctionDetector.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/idea/MainFunctionDetector.kt
@@ -125,7 +125,7 @@ class MainFunctionDetector {
             if (typeArguments.size != 1) return false
 
             val typeArgument = typeArguments[0].type
-            if (!(languageVersionSettings?.supportsFeature(LanguageFeature.AllowNullableArrayArgsInMain) &&
+            if (!(languageVersionSettings.supportsFeature(LanguageFeature.AllowNullableArrayArgsInMain) &&
                         KotlinBuiltIns.isStringOrNullableString(typeArgument) || KotlinBuiltIns.isString(typeArgument))
             ) {
                 return false
