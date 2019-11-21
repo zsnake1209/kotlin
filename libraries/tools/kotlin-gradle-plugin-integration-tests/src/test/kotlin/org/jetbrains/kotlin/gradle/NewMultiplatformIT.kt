2de68710a8cb0aef593d33027a1f2fba61b5da0b
sergey.igushkin <hotkeytlt@gmail.com>
2019-11-21 17:30:43 +0300

    Fix failure with Kotlin/Native distributions that have no stdlib
    Add a warning message during Gradle configuration phase if the
    Kotlin/Native distribution is missing the stdlib.
    
    Issue #KT-35033 Fixed

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/NewMultiplatformIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/NewMultiplatformIT.kt
@@ -4,9 +4,7 @@
  */
 package org.jetbrains.kotlin.gradle
 
-import org.jetbrains.kotlin.gradle.internals.DISABLED_NATIVE_TARGETS_REPORTER_DISABLE_WARNING_PROPERTY_NAME
-import org.jetbrains.kotlin.gradle.internals.DISABLED_NATIVE_TARGETS_REPORTER_WARNING_PREFIX
-import org.jetbrains.kotlin.gradle.internals.GRADLE_NO_METADATA_WARNING
+import org.jetbrains.kotlin.gradle.internals.*
 import org.jetbrains.kotlin.gradle.plugin.ProjectLocalConfigurations
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmWithJavaTargetPreset
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinMultiplatformPlugin
@@ -1486,6 +1484,8 @@ class NewMultiplatformIT : BaseGradleIT() {
                 assertSuccessful()
                 assertContains("User-provided Kotlin/Native distribution: $currentDir")
                 assertNotContains("Project property 'org.jetbrains.kotlin.native.home' is deprecated")
+                assertContains(NO_NATIVE_STDLIB_WARNING)
+                assertContains(NO_NATIVE_STDLIB_PROPERTY_WARNING)
             }
 
             // Deprecated property.
@@ -1493,6 +1493,8 @@ class NewMultiplatformIT : BaseGradleIT() {
                 assertSuccessful()
                 assertContains("User-provided Kotlin/Native distribution: $currentDir")
                 assertContains("Project property 'org.jetbrains.kotlin.native.home' is deprecated")
+                assertContains(NO_NATIVE_STDLIB_WARNING)
+                assertContains(NO_NATIVE_STDLIB_PROPERTY_WARNING)
             }
 
             build("tasks", "-Pkotlin.native.version=1.3-eap-10779") {
