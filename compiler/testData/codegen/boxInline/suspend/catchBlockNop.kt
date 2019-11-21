cf543498cee4be4592cd83ae0ca548276e5548b9
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-11-21 17:54:20 +0300

    Temporary comment out failing assert
     #KT-34999 Fixed
     #KT-35035 Open

---
+++ compiler/testData/codegen/boxInline/suspend/catchBlockNop.kt
@@ -0,0 +1,41 @@
+// IGNORE_BACKEND_MULTI_MODULE: JVM_IR
+// FILE: test.kt
+// WITH_RUNTIME
+// WITH_COROUTINES
+// NO_CHECK_LAMBDA_INLINING
+
+const val DEBUG = false
+inline fun inlineFun(b: () -> Unit) {
+    if (DEBUG) {
+        inlineFunReal(b)
+    }
+}
+
+inline fun inlineFunReal(b: () -> Unit) {
+    try {
+        b()
+    } finally {
+    }
+}
+
+// FILE: box.kt
+fun builder(c: suspend () -> Unit) {}
+
+class Sample {
+    fun test() {
+        inlineFun {
+            builder {
+                inlineFun {
+                    suspendFun()
+                }
+            }
+        }
+    }
+
+    suspend fun suspendFun() {}
+}
+
+fun box(): String {
+    Sample().test()
+    return "OK"
+}
