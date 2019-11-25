f23511d8aa77bc5760279678727390883183f676
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-11-25 08:38:38 +0100

    Use descriptor from resolved call to inline accessors
      Otherwise it causes VerifyError

--- compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -14521,16 +14521,6 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
     @TestDataPath("$PROJECT_ROOT")
     @RunWith(JUnit3RunnerWithInners.class)
     public static class Jvm8 extends AbstractLightAnalysisModeTest {
-        @TestMetadata("kt29242.kt")
-        public void ignoreKt29242() throws Exception {
-            runTest("compiler/testData/codegen/box/jvm8/kt29242.kt");
-        }
-
-        @TestMetadata("kt33054.kt")
-        public void ignoreKt33054() throws Exception {
-            runTest("compiler/testData/codegen/box/jvm8/kt33054.kt");
-        }
-
         private void runTest(String testDataFilePath) throws Exception {
             KotlinTestUtils.runTest(this::doTest, TargetBackend.JVM, testDataFilePath);
         }
@@ -14602,6 +14592,16 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
         @TestMetadata("kt16588.kt")
         public void testKt16588() throws Exception {
             runTest("compiler/testData/codegen/box/jvm8/kt16588.kt");
+        }
+
+        @TestMetadata("kt29242.kt")
+        public void testKt29242() throws Exception {
+            runTest("compiler/testData/codegen/box/jvm8/kt29242.kt");
+        }
+
+        @TestMetadata("kt33054.kt")
+        public void testKt33054() throws Exception {
+            runTest("compiler/testData/codegen/box/jvm8/kt33054.kt");
         }
 
         @TestMetadata("kt6301.kt")
