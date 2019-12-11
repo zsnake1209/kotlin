7bafe6f0665fc480180412420122fa322d55bc3b
Sergey Igushkin <hotkeytlt@gmail.com>
2019-12-11 16:14:49 +0300

    Support generating a klib from common code with K2MetadataCompiler

--- compiler/cli/cli-common/build.gradle.kts
+++ compiler/cli/cli-common/build.gradle.kts
@@ -9,6 +9,9 @@ dependencies {
     compile(project(":compiler:frontend"))
     compile(project(":compiler:frontend.java"))
     compile(project(":js:js.frontend"))
+    compile(project(":kotlin-util-klib"))
+    compile(project(":kotlin-util-klib-metadata"))
+    compile(project(":compiler:ir.serialization.common"))
     compileOnly(project(":kotlin-reflect-api"))
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     compileOnly(intellijDep()) { includeIntellijCoreJarDependencies(project) }
