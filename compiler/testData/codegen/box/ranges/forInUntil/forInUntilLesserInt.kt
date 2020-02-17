b9c02aa200def83ddbb147f71cb56f9224ec9fa7
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-02-17 14:11:45 +0300

    ~~~ Temporary BE test change ~~~

--- compiler/testData/codegen/box/ranges/forInUntil/forInUntilLesserInt.kt
+++ compiler/testData/codegen/box/ranges/forInUntil/forInUntilLesserInt.kt
@@ -2,9 +2,13 @@
 
 import kotlin.test.assertEquals
 
-fun box(): String {
+fun strangeLoop() {
     for (i in 10 until 0) {
         throw AssertionError("This loop should not be executed")
     }
+}
+
+fun box(): String {
+    strangeLoop()
     return "OK"
 }
