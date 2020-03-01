81cdaf9f5c1170c230fea4c7e00713a3110992ca
Victor Petukhov <victor.petukhov@jetbrains.com>
2020-03-02 01:21:23 +0300

    1

---
+++ compiler/testData/diagnostics/tests/inference/extensionLambdasAndArrow.kt
@@ -0,0 +1,11 @@
+// !DIAGNOSTICS: -UNUSED_VARIABLE -UNUSED_ANONYMOUS_PARAMETER
+
+fun main() {
+    val x1: String.() -> String = if (true) {{ this }} else {{ this }}
+    val x2: String.() -> String = if (true) {{ -> this }} else {{ -> this }}
+    val x3: () -> String = if (true) {{ -> "this" }} else {{ -> "this" }}
+    val x4: String.() -> String = if (true) {{ str: String -> "this" }} else {{ str: String -> "this" }}
+
+    val x5: String.() -> String = if (true) <!TYPE_MISMATCH!>{{ <!CANNOT_INFER_PARAMETER_TYPE, EXPECTED_PARAMETERS_NUMBER_MISMATCH!>str<!> -> "this" }}<!> else <!TYPE_MISMATCH!>{{ <!CANNOT_INFER_PARAMETER_TYPE, EXPECTED_PARAMETERS_NUMBER_MISMATCH!>str<!> -> "this" }}<!>
+    val x6: String.() -> String = if (true) <!TYPE_MISMATCH!>{{ <!CANNOT_INFER_PARAMETER_TYPE, EXPECTED_PARAMETERS_NUMBER_MISMATCH!>str<!> -> "this" }}<!> else {{ "this" }}
+}
