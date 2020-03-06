e62423c84448016bfdc207beb50bebe081817aea
simon.ogorodnik <simon.ogorodnik@jetbrains.com>
2020-03-06 20:55:23 +0300

    WIP: Type argument mapping

--- compiler/testData/diagnostics/tests/j+k/genericConstructor/superCallImpossibleToInfer.fir.kt
+++ compiler/testData/diagnostics/tests/j+k/genericConstructor/superCallImpossibleToInfer.fir.kt
@@ -12,7 +12,7 @@ public class A<E> {
 // TODO: It's effectively impossible to perform super call to such constructor
 // if there is not enough information to infer corresponding arguments
 // May be we could add some special syntax for such arguments
-class B1(x: List<String>) : A<CharSequence>("", x)
+class B1(x: List<String>) : <!INAPPLICABLE_CANDIDATE!>A<CharSequence><!>("", x)
 class B2(x: List<Int>) : <!INAPPLICABLE_CANDIDATE!>A<CharSequence><!>("", x)
 
 class C : A<CharSequence> {
