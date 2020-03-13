d729acd4bccc698640ab3827d28518b55fedf473
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-03-13 10:58:10 +0300

    [Gradle, JS] Recursive call of resolver

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/NpmResolverPlugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/NpmResolverPlugin.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.gradle.targets.js.npm
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ProjectDependency
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 
 class NpmResolverPlugin : Plugin<Project> {
@@ -16,23 +17,29 @@ class NpmResolverPlugin : Plugin<Project> {
         val nodeJs = NodeJsRootPlugin.apply(project)
         nodeJs.npmResolutionManager.requireConfiguringState().addProject(project)
 
-        project.configurations.all { configuration ->
-            configuration.allDependencies.all { dependency ->
-                if (dependency is ProjectDependency) {
-                    with(dependency.dependencyProject) {
-                        plugins.withType(NpmResolverPlugin::class.java) {
-                            nodeJs.npmResolutionManager.requireConfiguringState().addProject(this)
-                        }
-                    }
-
-                }
-            }
-        }
+        project.processProjectDependencies(nodeJs)
     }
 
     companion object {
         fun apply(project: Project) {
             project.plugins.apply(NpmResolverPlugin::class.java)
+        }
+    }
+}
+
+private fun Project.processProjectDependencies(nodeJs: NodeJsRootExtension) {
+    configurations.all { configuration ->
+        configuration.allDependencies.all { dependency ->
+            if (dependency is ProjectDependency) {
+                with(dependency.dependencyProject) {
+                    plugins.withType(NpmResolverPlugin::class.java) {
+                        val dependentNodeJs = NodeJsRootPlugin.apply(this)
+                        val dependentResolver = dependentNodeJs.npmResolutionManager.requireConfiguringState()[this]
+                        nodeJs.npmResolutionManager.requireConfiguringState().addProject(this, dependentResolver)
+                    }
+                    processProjectDependencies(nodeJs)
+                }
+            }
         }
     }
 }
