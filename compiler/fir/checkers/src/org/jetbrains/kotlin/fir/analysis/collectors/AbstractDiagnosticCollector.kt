93577422ece011d1ad970dd7b7b9537b4a0432ad
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-03-24 19:33:34 +0300

    Replace some FIR syntax errors with more proper diagnostics

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/collectors/AbstractDiagnosticCollector.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/collectors/AbstractDiagnosticCollector.kt
@@ -12,6 +12,10 @@ import org.jetbrains.kotlin.fir.analysis.collectors.components.*
 import org.jetbrains.kotlin.fir.analysis.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirDiagnostic
 import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.expressions.FirBreakExpression
+import org.jetbrains.kotlin.fir.expressions.FirContinueExpression
+import org.jetbrains.kotlin.fir.expressions.FirErrorLoop
+import org.jetbrains.kotlin.fir.expressions.FirLoopJump
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.resolve.SessionHolder
 import org.jetbrains.kotlin.fir.resolve.collectImplicitReceivers
@@ -66,11 +70,39 @@ abstract class AbstractDiagnosticCollector(
             element.acceptChildren(this)
         }
 
+        private fun visitJump(loopJump: FirLoopJump) {
+            loopJump.runComponents()
+            loopJump.acceptChildren(this)
+            loopJump.target.labeledElement.takeIf { it is FirErrorLoop }?.accept(this)
+        }
+
+        override fun visitBreakExpression(breakExpression: FirBreakExpression) {
+            visitJump(breakExpression)
+        }
+
+        override fun visitContinueExpression(continueExpression: FirContinueExpression) {
+            visitJump(continueExpression)
+        }
+
         override fun visitRegularClass(regularClass: FirRegularClass) {
             val typeRef = buildResolvedTypeRef {
                 type = regularClass.defaultType()
             }
             visitWithDeclarationAndReceiver(regularClass, regularClass.name, typeRef)
+        }
+
+        override fun visitSealedClass(sealedClass: FirSealedClass) {
+            val typeRef = buildResolvedTypeRef {
+                type = sealedClass.defaultType()
+            }
+            visitWithDeclarationAndReceiver(sealedClass, sealedClass.name, typeRef)
+        }
+
+        override fun visitAnonymousObject(anonymousObject: FirAnonymousObject) {
+            val typeRef = buildResolvedTypeRef {
+                type = anonymousObject.defaultType()
+            }
+            visitWithDeclarationAndReceiver(anonymousObject, null, typeRef)
         }
 
         override fun visitSimpleFunction(simpleFunction: FirSimpleFunction) {
