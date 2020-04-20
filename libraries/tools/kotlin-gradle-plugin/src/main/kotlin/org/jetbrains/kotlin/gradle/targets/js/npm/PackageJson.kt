29bd547e78f0c5b83d05c815b625d01ad8ce87cd
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-04-20 13:34:05 +0300

    [Gradle, JS] Move suppress gson null issue for particular fields

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/PackageJson.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/PackageJson.kt
@@ -1,6 +1,6 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
- * that can be found in the license/LICENSE.txt file.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.gradle.targets.js.npm
@@ -10,8 +10,7 @@ import com.google.gson.GsonBuilder
 import org.jetbrains.kotlin.gradle.internal.ensureParentDirsCreated
 import java.io.File
 
-// Gson set nulls reflective no matter on default values and non-null types
-@Suppress("USELESS_ELVIS")
+// Gson set nulls reflectively no matter on default values and non-null types
 class PackageJson(
     var name: String,
     var version: String
@@ -32,18 +31,23 @@ class PackageJson(
 
     var workspaces: Collection<String>? = null
 
+    @Suppress("USELESS_ELVIS")
     val devDependencies = mutableMapOf<String, String>()
         get() = field ?: mutableMapOf()
 
+    @Suppress("USELESS_ELVIS")
     val dependencies = mutableMapOf<String, String>()
         get() = field ?: mutableMapOf()
 
+    @Suppress("USELESS_ELVIS")
     val peerDependencies = mutableMapOf<String, String>()
         get() = field ?: mutableMapOf()
 
+    @Suppress("USELESS_ELVIS")
     val optionalDependencies = mutableMapOf<String, String>()
         get() = field ?: mutableMapOf()
 
+    @Suppress("USELESS_ELVIS")
     val bundledDependencies = mutableListOf<String>()
         get() = field ?: mutableListOf()
 
