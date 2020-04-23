06fb6ed91caabf44131f8eb0e3cda7e0183dbdb5
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-04-24 02:48:31 +0300

    Overload resolution by lambda return type
    Introduce annotation to enable overload resolution by lambda
    return type in kotlin package and make compiler recognize it.
    
    Change overload selection algorithm to consider all overloads
    with that annotation, not just the first one.

--- libraries/stdlib/src/kotlin/annotations/Inference.kt
+++ libraries/stdlib/src/kotlin/annotations/Inference.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -35,3 +35,10 @@ import kotlin.experimental.ExperimentalTypeInference
 @SinceKotlin("1.3")
 @ExperimentalTypeInference
 public annotation class BuilderInference
+
+
+//TODO: @Target
+@Retention(AnnotationRetention.BINARY)
+@SinceKotlin("1.4")
+@ExperimentalTypeInference
+public annotation class OverloadResolutionByLambdaReturnType
