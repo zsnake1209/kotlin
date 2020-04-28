3af5a548bb78a2b0974d31fc0e8a049cb77231ef
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-04-28 11:27:41 +0300

    [FIR] Add `transformDeclarations` to FirFile

--- compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/NodeConfigurator.kt
+++ compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/NodeConfigurator.kt
@@ -389,7 +389,7 @@ object NodeConfigurator : AbstractFieldConfigurator<FirTreeBuilder>(FirTreeBuild
 
         file.configure {
             +fieldList(import)
-            +declarations
+            +declarations.withTransform()
             +stringField("name")
             +field("packageFqName", fqNameType)
         }
