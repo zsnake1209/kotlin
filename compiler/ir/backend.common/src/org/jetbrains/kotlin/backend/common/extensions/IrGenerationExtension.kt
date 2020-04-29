764066ac499cbb7405712e414b8eaa94ec184dbf
Roman Artemev <roman.artemev@jetbrains.com>
2020-04-29 19:26:22 +0300

    [IR] Deprecate some API

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/extensions/IrGenerationExtension.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/extensions/IrGenerationExtension.kt
@@ -24,11 +24,14 @@ import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.scopes.MemberScope
 
-class IrPluginContext(
+open class IrPluginContext(
+    @Deprecated("FrontEnd API shouldn't be accessed in Ir plugin environment")
     val moduleDescriptor: ModuleDescriptor,
+    @Deprecated("FrontEnd API shouldn't be accessed in Ir plugin environment")
     val bindingContext: BindingContext,
     val languageVersionSettings: LanguageVersionSettings,
     val symbolTable: SymbolTable,
+    @Deprecated("FrontEnd API shouldn't be accessed in Ir plugin environment")
     val typeTranslator: TypeTranslator,
     override val irBuiltIns: IrBuiltIns,
     private val linker: IrDeserializer,
