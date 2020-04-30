b36f1a9398c70c77da4c4ba5f72abe5b5ea4e627
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-04-30 13:40:25 +0300

    KotlinDslModels: project can be null in ExternalSystemTaskNotificationListener.onStart

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
@@ -13,28 +13,49 @@ import org.jetbrains.kotlin.idea.framework.GRADLE_SYSTEM_ID
 import org.jetbrains.kotlin.idea.scripting.gradle.GradleScriptDefinitionsContributor
 
 class KotlinDslSyncListener : ExternalSystemTaskNotificationListenerAdapter() {
+    private val workingDirs = hashMapOf<ExternalSystemTaskId, String>()
+
     override fun onStart(id: ExternalSystemTaskId, workingDir: String?) {
         if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
         if (id.projectSystemId != GRADLE_SYSTEM_ID) return
-
         if (workingDir == null) return
+
+        workingDirs[id] = workingDir
+
         val project = id.findProject() ?: return
 
         gradleState.isSyncInProgress = true
-        project.kotlinGradleDslSync[id] = KotlinDslGradleBuildSync(workingDir, id)
+
+        project.kotlinGradleDslSync[id] = KotlinDslGradleBuildSync(id)
     }
 
     override fun onEnd(id: ExternalSystemTaskId) {
         if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
         if (id.projectSystemId != GRADLE_SYSTEM_ID) return
+        val workingDir = workingDirs.remove(id) ?: return
 
         val project = id.findProject() ?: return
 
         gradleState.isSyncInProgress = false
+
         ScriptDefinitionContributor.find<GradleScriptDefinitionsContributor>(project)?.reloadIfNecessary()
 
         val sync = project.kotlinGradleDslSync.remove(id) ?: return
+        sync.workingDir = workingDir
+
         saveScriptModels(project, sync)
+    }
+
+    override fun onCancel(id: ExternalSystemTaskId) {
+        if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
+        if (id.projectSystemId != GRADLE_SYSTEM_ID) return
+
+        val project = id.findProject() ?: return
+
+        gradleState.isSyncInProgress = true
+
+        project.kotlinGradleDslSync.remove(id)
+        workingDirs.remove(id)
     }
 
     companion object {
