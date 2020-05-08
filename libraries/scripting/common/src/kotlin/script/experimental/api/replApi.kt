c488e84deb067e19231e93c91b979a790a4e4670
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-05-08 14:07:54 +0200

    [minor] refactor repl API file layout

--- libraries/scripting/common/src/kotlin/script/experimental/api/replApi.kt
+++ libraries/scripting/common/src/kotlin/script/experimental/api/replApi.kt
@@ -15,16 +15,6 @@ import kotlin.script.experimental.util.LinkedSnippet
 typealias CompiledSnippet = CompiledScript
 
 /**
- * Type for [ReplCompleter.complete] return value
- */
-typealias ReplCompletionResult = Sequence<SourceCodeCompletionVariant>
-
-/**
- * Type for [ReplCodeAnalyzer.analyze] return value
- */
-typealias ReplAnalyzerResult = Sequence<ScriptDiagnostic>
-
-/**
  * REPL Compiler interface which is used for compiling snippets
  * @param CompiledSnippetT Implementation of [CompiledSnippet] which is returned by this compiler
  */
@@ -57,55 +47,6 @@ interface ReplCompiler<CompiledSnippetT : CompiledSnippet> {
         snippet: SourceCode,
         configuration: ScriptCompilationConfiguration
     ): ResultWithDiagnostics<LinkedSnippet<CompiledSnippetT>> = compile(listOf(snippet), configuration)
-}
-
-/**
- * Single code completion result
- */
-data class SourceCodeCompletionVariant(
-    val text: String,
-    val displayText: String,
-    val tail: String,
-    val icon: String
-)
-
-/**
- * Interface for REPL context completion
- */
-interface ReplCompleter {
-
-    /**
-     * Returns the list of possible reference variants in [cursor] position.
-     * Generally <b>doesn't change</b> the internal state of implementing object.
-     * @param snippet Completion context
-     * @param cursor Cursor position in which completion variants should be calculated
-     * @param configuration Compilation configuration which is used. Script should be analyzed, but code generation is not performed
-     * @return List of reference variants
-     */
-    suspend fun complete(
-        snippet: SourceCode,
-        cursor: SourceCode.Position,
-        configuration: ScriptCompilationConfiguration
-    ): ResultWithDiagnostics<ReplCompletionResult>
-}
-
-/**
- * Interface for REPL syntax and semantic analysis
- */
-interface ReplCodeAnalyzer {
-
-    /**
-     * Reports compilation errors and warnings in the given [snippet]
-     * @param snippet Code to analyze
-     * @param cursor Current cursor position. May be used by implementation for suppressing errors and warnings near it.
-     * @param configuration Compilation configuration which is used. Script should be analyzed, but code generation is not performed
-     * @return List of diagnostic messages
-     */
-    suspend fun analyze(
-        snippet: SourceCode,
-        cursor: SourceCode.Position,
-        configuration: ScriptCompilationConfiguration
-    ): ResultWithDiagnostics<ReplAnalyzerResult>
 }
 
 /**
