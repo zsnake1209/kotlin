cbd10349fd78ae6512db7100ffb85f06fdd81a20
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-05-08 22:32:05 +0900

    ~~~~ switch 202 ~~~~

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/GradleImportHelper.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/GradleImportHelper.kt
@@ -7,97 +7,80 @@
 
 package org.jetbrains.kotlin.idea.scripting.gradle
 
-import com.intellij.notification.*
+import com.intellij.diff.util.DiffUtil
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
+import com.intellij.testFramework.LightVirtualFileBase
 import org.jetbrains.kotlin.idea.KotlinIcons
 import org.jetbrains.kotlin.idea.KotlinIdeaGradleBundle
+import org.jetbrains.kotlin.idea.scripting.gradle.importing.KotlinDslScriptModelResolver
 import org.jetbrains.kotlin.psi.UserDataProperty
-import org.jetbrains.plugins.gradle.settings.GradleProjectSettings
+import org.jetbrains.plugins.gradle.service.project.GradlePartialResolverPolicy
+import org.jetbrains.plugins.gradle.service.project.GradleProjectResolverExtension
 import org.jetbrains.plugins.gradle.util.GradleConstants
+import java.util.function.Predicate
 
 fun runPartialGradleImport(project: Project) {
     getGradleProjectSettings(project).forEach {
         ExternalSystemUtil.refreshProject(
             it.externalProjectPath,
             ImportSpecBuilder(project, GradleConstants.SYSTEM_ID)
-                .build()
+                .projectResolverPolicy(
+                    GradlePartialResolverPolicy(Predicate<GradleProjectResolverExtension?> { it is KotlinDslScriptModelResolver })
+                )
         )
     }
 }
 
-fun getMissingConfigurationNotificationText() = KotlinIdeaGradleBundle.message("script.configurations.will.be.available.after.import")
-fun getMissingConfigurationActionText() = KotlinIdeaGradleBundle.message("action.label.import.project")
+fun getMissingConfigurationNotificationText() = KotlinIdeaGradleBundle.message("script.configurations.will.be.available.after.load.changes")
+fun getMissingConfigurationActionText() = KotlinIdeaGradleBundle.message("action.text.load.script.configurations")
 
-private const val kotlinDslNotificationGroupId = "Gradle Kotlin DSL Scripts"
-private var Project.notificationPanel: ScriptConfigurationChangedNotification?
-        by UserDataProperty<Project, ScriptConfigurationChangedNotification>(Key.create("load.script.configuration.panel"))
+fun showNotificationForProjectImport(project: Project) {}
 
+fun hideNotificationForProjectImport(project: Project): Boolean = true
 
-fun showNotificationForProjectImport(project: Project) {
-    val existingPanel = project.notificationPanel
-    if (existingPanel != null) {
-        return
+class LoadConfigurationAction : AnAction(
+    KotlinIdeaGradleBundle.message("action.text.load.script.configurations"),
+    KotlinIdeaGradleBundle.message("action.description.load.script.configurations"),
+    KotlinIcons.LOAD_SCRIPT_CONFIGURATION
+) {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        runPartialGradleImport(project)
     }
 
-    val notificationGroup = NotificationGroup.findRegisteredGroup(kotlinDslNotificationGroupId)
-    if (notificationGroup == null) {
-        NotificationsConfiguration.getNotificationsConfiguration().register(
-            kotlinDslNotificationGroupId, NotificationDisplayType.STICKY_BALLOON, false
-        )
+    override fun update(e: AnActionEvent) {
+        ensureValidActionVisibility(e)
+
+        e.presentation.description = KotlinIdeaGradleBundle.message("action.description.load.script.configurations")
     }
 
-    val notification = ScriptConfigurationChangedNotification(project)
-    project.notificationPanel = notification
-    notification.notify(project)
-}
+    private fun ensureValidActionVisibility(e: AnActionEvent) {
+        val editor = e.getData(CommonDataKeys.EDITOR) ?: return
 
-fun hideNotificationForProjectImport(project: Project): Boolean {
-    if (project.notificationPanel == null) return false
-    project.notificationPanel?.expire()
-    return true
-}
-
-private class ScriptConfigurationChangedNotification(val project: Project) :
-    Notification(
-        kotlinDslNotificationGroupId,
-        KotlinIcons.LOAD_SCRIPT_CONFIGURATION,
-        KotlinIdeaGradleBundle.message("notification.title.script.configuration.has.been.changed"),
-        null,
-        KotlinIdeaGradleBundle.message("notification.text.script.configuration.has.been.changed"),
-        NotificationType.INFORMATION,
-        null
-    ) {
-
-    init {
-        addAction(LoadConfigurationAction())
-        addAction(NotificationAction.createSimple(KotlinIdeaGradleBundle.message("action.label.enable.auto.import")) {
-            val gradleSettings = ExternalSystemApiUtil.getSettings(project, GradleConstants.SYSTEM_ID)
-            val projectSettings = gradleSettings.getLinkedProjectsSettings()
-                .filterIsInstance<GradleProjectSettings>()
-                .firstOrNull()
-            if (projectSettings != null) {
-                projectSettings.isUseAutoImport = true
-            }
-            runPartialGradleImport(project)
-        })
-    }
-
-    override fun expire() {
-        super.expire()
-
-        project.notificationPanel = null
-    }
-
-    private class LoadConfigurationAction : AnAction(KotlinIdeaGradleBundle.message("action.label.import.project")) {
-        override fun actionPerformed(e: AnActionEvent) {
-            val project = e.project ?: return
-            runPartialGradleImport(project)
+        if (DiffUtil.isDiffEditor(editor)) {
+            e.presentation.isVisible = false
+            return
         }
+
+        e.presentation.isVisible = editor.getNotificationVisibility()
+    }
+
+    private fun Editor.getNotificationVisibility(): Boolean {
+        val project = project ?: return false
+
+        val documentManager = FileDocumentManager.getInstance()
+        val virtualFile = documentManager.getFile(document)
+        if (virtualFile is LightVirtualFileBase) return false
+        if (virtualFile == null || !virtualFile.isValid) return false
+
+        return GradleScriptingSupportProvider.getInstance(project).isConfigurationOutOfDate(virtualFile)
     }
 }
