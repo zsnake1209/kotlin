c9601ee2fe01612ae1f4fb0cdb64c0d6745dd736
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-05-12 17:37:17 +0300

    Fix not running tests

--- idea/idea-core/build.gradle.kts
+++ idea/idea-core/build.gradle.kts
@@ -29,8 +29,6 @@ dependencies {
     compile(commonDep("org.jetbrains.kotlinx", "kotlinx-coroutines-core")) { isTransitive = false }
     compile(commonDep("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8")) { isTransitive = false }
 
-    compile(project(":idea:idea-frontend-fir"))
-
     compileOnly(intellijDep())
 
     Platform[192].orHigher {
