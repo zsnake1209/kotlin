dc96365d0837e344077f5eb054512b8a0021f28f
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2020-05-13 13:34:33 +0300

    Got rid of LazyIrProvider. It is not used anymore

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/SymbolTable.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/SymbolTable.kt
@@ -36,16 +36,6 @@ interface IrProvider {
     fun getDeclaration(symbol: IrSymbol): IrDeclaration?
 }
 
-/**
- * Extension of [IrProvider] which always produces inheritors of [IrLazyDeclarationBase].
- * Thus, it needs [declarationStubGenerator] to be able to produce IR declarations.
- */
-interface LazyIrProvider : IrProvider {
-    var declarationStubGenerator: DeclarationStubGenerator
-
-    override fun getDeclaration(symbol: IrSymbol): IrLazyDeclarationBase?
-}
-
 interface IrExtensionGenerator {
     fun declare(symbol: IrSymbol): IrDeclaration? = null
 }
